a39aec6f0ef628c5c39319851ef5f4afd342b264
Marius Pachitariu <marius10p@gmail.com>
2018-07-19 15:10:47 -0400

    added multi-plane, multi-channel

--- register.py
+++ register.py
@@ -1,5 +1,7 @@
+from skimage import io
+import glob 
 import numpy as np
-import time
+import time, os
 import matplotlib.pyplot as plt
 from numpy import fft
 from numpy import random as rnd
@@ -15,8 +17,7 @@ maskSlope   = 2. # slope of taper mask at the edges
 # smoothing kernel
 def kernelD(a, b):        
     dxs = np.reshape(a[0], (-1,1)) - np.reshape(b[0], (1,-1))
-    dys = np.reshape(a[1], (-1,1)) - np.reshape(b[1], (1,-1))
-    
+    dys = np.reshape(a[1], (-1,1)) - np.reshape(b[1], (1,-1))    
     ds = np.square(dxs) + np.square(dys)
     K = np.exp(-ds/(2*np.square(sigL)))    
     return K
@@ -24,54 +25,36 @@ def kernelD(a, b):
 def mat_upsample(lpad, ops):    
     lar    = np.arange(-lpad, lpad+1)
     larUP  = np.arange(-lpad, lpad+.001, 1./ops['subpixel'])
-
     x, y   = np.meshgrid(lar, lar)   
     xU, yU = np.meshgrid(larUP, larUP)   
-
     Kx = kernelD((x,y),(x,y))
     Kx = np.linalg.inv(Kx)
-    Kg = kernelD((x,y),(xU,yU))
-    
+    Kg = kernelD((x,y),(xU,yU))    
     Kmat = np.dot(Kx, Kg)
-    nup = larUP.shape[0]
-    
+    nup = larUP.shape[0]    
     return Kmat, nup
 
-
 def prepareMasks(refImg):    
-    i0,Ly,Lx = refImg.shape
-    
+    i0,Ly,Lx = refImg.shape    
     x = np.arange(0, Lx)
     y = np.arange(0, Ly)
     x = np.abs(x - x.mean())
     y = np.abs(y - y.mean())
-
     xx, yy = np.meshgrid(x, y)
-
     mY = y.max() - 4.
-    mX = x.max() - 4.
-    
+    mX = x.max() - 4.    
     maskY = 1./(1.+np.exp((yy-mY)/maskSlope))
     maskX = 1./(1.+np.exp((xx-mX)/maskSlope))
     maskMul = maskY * maskX
     maskOffset = refImg.mean() * (1. - maskMul);
-
-    #plt.imshow(maskMul)
-    #plt.show()
-    #plt.imshow(maskOffset)
-    #plt.show()
-    
     hgx = np.exp(-np.square(xx/smoothSigma))
     hgy = np.exp(-np.square(yy/smoothSigma))
     hgg = hgy * hgx
     hgg = hgg/hgg.sum()
-
     fhg = np.real(fft.fft2(fft.ifftshift(hgg))); # smoothing filter in Fourier domain
-
     cfRefImg   = np.conj(fft.fft2(refImg));
     absRef     = np.absolute(cfRefImg);
-    cfRefImg   = cfRefImg / (eps0 + absRef) * fhg;
-    
+    cfRefImg   = cfRefImg / (eps0 + absRef) * fhg;    
     return maskMul, maskOffset, cfRefImg
 
 def correlation_map(data, cfRefImg):
@@ -83,113 +66,317 @@ def correlation_map(data, cfRefImg):
     return cc, X
 
 def getXYup(cc, Ls, ops):
-    (lcorr, lpad, Lyhalf, Lxhalf) = Ls
-    
+    (lcorr, lpad, Lyhalf, Lxhalf) = Ls    
     nimg = cc.shape[0]
     cc0 = cc[:, (Lyhalf-lcorr):(Lyhalf+lcorr+1), (Lxhalf-lcorr):(Lxhalf+lcorr+1)]
-
     cc0 = np.reshape(cc0, (nimg, -1))
     ix  = np.argmax(cc0, axis = 1)
-
     ymax, xmax = np.unravel_index(ix, (2*lcorr+1,2*lcorr+1))
-    mxpt = [ymax+Lyhalf-lcorr, xmax + Lxhalf-lcorr]
-    
+    mxpt = [ymax+Lyhalf-lcorr, xmax + Lxhalf-lcorr]    
     ccmat = np.zeros((nimg, 2*lpad+1, 2*lpad+1))
     for j in range(0, nimg):
         ccmat[j,:,:] = cc[j, (mxpt[0][j] -lpad):(mxpt[0][j] +lpad+1), (mxpt[1][j] -lpad):(mxpt[1][j] +lpad+1)]
-
     ccmat = np.reshape(ccmat, (nimg,-1))
-
     Kmat, nup = mat_upsample(lpad, ops)
-
     ccb = np.dot(ccmat, Kmat)
     imax = np.argmax(ccb, axis=1)
     cmax = np.amax(ccb, axis=1)
-
     ymax, xmax = np.unravel_index(imax, (nup,nup))
-
-    mdpt = np.floor(nup/2)
-    
-    ymax,xmax = (ymax-mdpt)/ops['subpixel'], (xmax-mdpt)/ops['subpixel']
-    
+    mdpt = np.floor(nup/2)    
+    ymax,xmax = (ymax-mdpt)/ops['subpixel'], (xmax-mdpt)/ops['subpixel']    
     ymax, xmax = ymax + mxpt[0] - Lyhalf, xmax + mxpt[1] - Lxhalf
+    return ymax, xmax, cmax
 
-    return ymax, xmax
-
-def shift_data(X, ymax,xmax):
-    nimg, Ly, Lx = X.shape
-    
+def shift_data(X, ymax,xmax, isfft):
+    if not isfft:
+        X = fft.fft2(X)            
+    nimg, Ly, Lx = X.shape    
     Ny = fft.ifftshift(np.arange(-np.fix(Ly/2), np.ceil(Ly/2)))
     Nx = fft.ifftshift(np.arange(-np.fix(Lx/2), np.ceil(Lx/2)))
     [Nx,Ny] = np.meshgrid(Nx,Ny)
     Nx = Nx / Lx
-    Ny = Ny / Ly
-    
-    dph = Nx * np.reshape(xmax, (-1,1,1)) + Ny * np.reshape(ymax, (-1,1,1))
-    
+    Ny = Ny / Ly    
+    dph = Nx * np.reshape(xmax, (-1,1,1)) + Ny * np.reshape(ymax, (-1,1,1))    
     Y = np.real(fft.ifft2(X * np.exp((2j * np.pi) * dph)))
     return Y
     
-def phasecorr_worker(data, refImg, ops):    
-    nimg, Ly, Lx = data.shape
-    refImg = np.reshape(refImg, (1, Ly, Lx))
-    
+def phasecorr_worker(data, refImg, ops, flag_zeroed):    
+    nimg, Ly, Lx = data.shape        
+    refImg = np.reshape(refImg, (1, Ly, Lx))    
     Lyhalf = int(np.floor(Ly/2))
-    Lxhalf = int(np.floor(Lx/2))
-    
+    Lxhalf = int(np.floor(Lx/2))    
     if ops['maxregshift']>0:
         maxregshift = ops['maxregshift'] 
     else:
         maxregshift = np.round(.1*np.maximum(Ly, Lx))
-
     lcorr = int(np.minimum(maxregshift, np.floor(np.minimum(Ly,Lx)/2.)-lpad))
-
     (maskMul, maskOffset, cfRefImg) = prepareMasks(refImg)
-
-    data = data * maskMul + maskOffset
-
+    data = data.astype('float32') * maskMul + maskOffset
     cc, X = correlation_map(data, cfRefImg)
+    ymax, xmax, cmax = getXYup(cc, (lcorr,lpad, Lyhalf, Lxhalf), ops)    
+    if flag_zeroed:
+        xmax = xmax - xmax.mean()
+        ymax = ymax - ymax.mean()        
+    Y = shift_data(X, ymax,xmax, True)    
+    return Y, ymax, xmax, cmax
 
-    ymax, xmax = getXYup(cc, (lcorr,lpad, Lyhalf, Lxhalf), ops)
-    
-    Y = shift_data(X, ymax,xmax)
-    
-    return Y, ymax, xmax
-
-def phasecorr(data, refImg, ops):    
-    nimg = data.shape[0]
-    
+def phasecorr(data, refImg, ops, flag_zeroed):        
     if ops['num_workers']<0:
         Y, ymax, xmax = phasecorr_worker(data, refImg, ops)
     else:
+        nimg = data.shape[0]    
+        if ops['num_workers']<1:
+            ops['num_workers'] = int(multiprocessing.cpu_count()/2)            
         num_cores = ops['num_workers']
-        if num_cores<1:
-            num_cores = multiprocessing.cpu_count()
-            
-        nbatch = int(np.ceil(nimg/float(num_cores)))
-    
+        
+        nbatch = int(np.ceil(nimg/float(num_cores)))    
         inputs = range(0, nimg, nbatch)
         irange = []
         for i in inputs:
             irange.append(i + np.arange(0,np.minimum(nbatch, nimg-i)))
-
-        results = Parallel(n_jobs=num_cores)(delayed(phasecorr_worker)(data[irange[j],:, :], refImg, ops) for j in range(0,len(irange)))
-        
+        #if __name__ == '__main__':
+        results = Parallel(n_jobs=num_cores)(delayed(phasecorr_worker)(data[irange[j],:, :], 
+                                                                       refImg, ops, flag_zeroed) for j in range(0,len(irange)))
         Y = np.zeros_like(data)
         ymax = np.zeros((nimg,))
-        xmax = np.zeros((nimg,))
+        xmax = np.zeros((nimg,))        
+        cmax = np.zeros((nimg,))        
         for i in range(0,len(results)):
             Y[irange[i], :, :] = results[i][0]
             ymax[irange[i]] = results[i][1]
-            xmax[irange[i]] = results[i][2]
-    
-    return Y, ymax, xmax
+            xmax[irange[i]] = results[i][2]            
+            cmax[irange[i]] = results[i][3]                
+    return Y, ymax, xmax, cmax
 
-def registerBinary(ops):
+def get_nFrames(ops):
+    nbytes = os.path.getsize(ops['reg_file'])
+    nFrames = int(nbytes/(2* ops['Ly'] *  ops['Lx']))
+    return nFrames
+
+def register_myshifts(ops, data, ymax, xmax):    
+    if ops['num_workers']<0:
+        dreg = shift_data(data, ymax, xmax, False)
+    else:        
+        if ops['num_workers']<1:
+            ops['num_workers'] = int(multiprocessing.cpu_count()/2)            
+        num_cores = ops['num_workers']       
+        nimg = data.shape[0]    
+        nbatch = int(np.ceil(nimg/float(num_cores)))    
+        inputs = range(0, nimg, nbatch)
+        irange = []
+        for i in inputs:
+            irange.append(i + np.arange(0,np.minimum(nbatch, nimg-i)))   
+        results = Parallel(n_jobs=num_cores)(delayed(shift_data)(data[irange[j],:, :], 
+                                                                 ymax[irange[j]], xmax[irange[j]], False) for j in range(0,len(irange)))   
+        dreg = np.zeros_like(data)
+        for i in range(0,len(results)):
+            dreg[irange[i], :, :] = results[i]        
+    return dreg
+
+def register_binary(ops):  
+    # if ops is a list of dictionaries, each will be registered separately
+    if type(ops) is list:
+        for op in ops:
+            op = register_binary(op)
+        return ops
     
+    Ly = ops['Ly']
+    Lx = ops['Lx']    
+    ops['nframes'] = get_nFrames(ops) 
+    refImg = pick_init(ops)    
+    nbatch = ops['batch_size']
+    nbytesread = 2 * Ly * Lx * nbatch
     
+    if ops['nchannels']>1 and ops['align_by_chan']>1:
+        reg_file = open(ops['reg_file_chan2'], 'r+b')    
+    else: 
+        reg_file = open(ops['reg_file'], 'r+b')    
+    yoff = []
+    xoff = []
+    corrXY = []    
+    meanImg = np.zeros((Ly, Lx))    
+    while True:
+        buff = reg_file.read(nbytesread)    
+        data = np.frombuffer(buff, dtype=np.int16, offset=0)
+        if data.size==0:
+            break            
+        data = np.reshape(data, (-1, Ly, Lx))
+        dwrite, ymax, xmax, cmax = phasecorr(data, refImg, ops, False)
+        dwrite = dwrite.astype('int16')
+        reg_file.seek(-2*dwrite.size,1)    
+        reg_file.write(bytearray(dwrite))        
+        meanImg += dwrite.sum(axis=0)
+        yoff = np.append(yoff, ymax)
+        xoff = np.append(xoff, xmax)
+        corrXY = np.append(corrXY, cmax)        
+    ops['yoff'] = yoff
+    ops['xoff'] = xoff
+    ops['corrXY'] = corrXY
+    ops['refImg'] = refImg    
+    if ops['align_by_chan']>1:
+        ops['meanImg_chan2'] = meanImg/ops['nframes']
+    else:
+        ops['meanImg'] = meanImg/ops['nframes']    
+    if ops['nchannels']>1:
+        ix = 0
+        meanImg = np.zeros((Ly, Lx))   
+        if ops['align_by_chan']>1:
+            reg_file = open(ops['reg_file_chan2'], 'r+b')    
+        else:
+            reg_file = open(ops['reg_file'], 'r+b')            
+        while True:
+            buff = reg_file.read(nbytesread)    
+            data = np.frombuffer(buff, dtype=np.int16, offset=0)
+            if data.size==0:
+                break            
+            data = np.reshape(data, (-1, Ly, Lx))
+            nframes = data.shape[0]
+            # register by pre-determined amount            
+            dwrite = register_myshifts(ops, data, yoff[ix + np.arange(0,nframes)], xoff[ix + np.arange(0,nframes)])
+            ix += nframes            
+            dwrite = dwrite.astype('int16')
+            reg_file.seek(-2*dwrite.size,1)    
+            reg_file.write(bytearray(dwrite))        
+            meanImg += dwrite.sum(axis=0)
+            yoff = np.append(yoff, ymax)
+            xoff = np.append(xoff, xmax)
+            corrXY = np.append(corrXY, cmax)                
+            if ops['align_by_chan']>1:
+                ops['meanImg'] = meanImg/ops['nframes']
+            else:
+                ops['meanImg_chan2'] = meanImg/ops['nframes']    
     
+    np.save(ops['ops_path'], ops)
     
+    return ops
+
+def subsample_frames(ops, nsamps):    
+    nFrames = get_nFrames(ops) 
+    Ly = ops['Ly']
+    Lx = ops['Lx']    
+    frames = np.zeros((nsamps, Ly, Lx), dtype='int16')    
+    nbytesread = 2 * Ly * Lx    
+    istart = np.linspace(0, nFrames, 1+nsamps).astype('int64')    
+    reg_file = open(ops['reg_file'], 'rb')        
+    for j in range(0,nsamps):        
+        reg_file.seek(nbytesread * istart[j], 0)  
+        buff = reg_file.read(nbytesread)        
+        data = np.frombuffer(buff, dtype=np.int16, offset=0)
+        frames[j,:,:] = np.reshape(data, (Ly, Lx))    
+    reg_file.close()    
+    return frames
     
-    
-    return 
+def pick_init_init(ops, frames):
+    nimg = frames.shape[0]
+    frames = np.reshape(frames, (nimg,-1)).astype('float32')
+    frames = frames - np.reshape(frames.mean(axis=1), (nimg, 1))
+    cc = frames @ np.transpose(frames)
+    ndiag = np.sqrt(np.diag(cc))
+    cc = cc / np.outer(ndiag, ndiag)
+    CCsort = -np.sort(-cc, axis = 1)
+    bestCC = np.mean(CCsort[:, 1:20], axis=1);
+    imax = np.argmax(bestCC)
+    indsort = np.argsort(-cc[imax, :])
+    refImg = np.mean(frames[indsort[0:20], :], axis = 0)
+    refImg = np.reshape(refImg, (ops['Ly'], ops['Lx']))    
+    return refImg
+
+def refine_init_init(ops, frames, refImg):    
+    niter = 8
+    nmax  = np.minimum(100, int(frames.shape[0]/2))    
+    for iter in range(0,niter):
+        freg, ymax, xmax, cmax = phasecorr(frames, refImg, ops, True)
+        isort = np.argsort(-cmax)
+        if iter>=niter/2:
+            nmax = int(frames.shape[0]/2)
+        refImg = np.mean(freg[isort[1:nmax], :, :], axis=0)    
+    return refImg
+
+def pick_init(ops):
+    nbytes = os.path.getsize(ops['reg_file'])
+    Ly = ops['Ly']
+    Lx = ops['Lx']    
+    nFrames = int(nbytes/(2*Ly*Lx))    
+    nFramesInit = np.minimum(ops['nimg_init'], nFrames)    
+    frames = subsample_frames(ops, nFramesInit)
+    refImg = pick_init_init(ops, frames)       
+    refImg = refine_init_init(ops, frames, refImg)    
+    return refImg
+
+def tiff_to_binary(ops):    
+    nplanes = ops['nplanes']
+    nchannels = ops['nchannels']
+    ops1 = []
+    # open all binary files for writing
+    reg_file = []
+    if nchannels>1:
+        reg_file_chan2 = []            
+    for j in range(0,nplanes):        
+        fpath = os.path.join(ops['data_path'], 'suite2p', 'plane%d'%j)
+        ops['save_path'] = fpath
+        ops['ops_path'] = os.path.join(fpath,'ops.npy')        
+        ops['reg_file'] = os.path.join(fpath, 'data.bin')        
+        if nchannels>1:
+            ops['reg_file_chan2'] = os.path.join(fpath, 'data_chan2.bin')
+        if not os.path.isdir(fpath):
+            os.makedirs(fpath)
+        ops1.append(ops.copy())                   
+        reg_file.append(open(ops['reg_file'], 'wb'))
+        if nchannels>1:
+            reg_file_chan2.append(open(ops['reg_file_chan2'], 'wb'))
+    fs = get_tif_list(ops) # look for tiffs in all requested folders
+    # keep track of the plane identity of the first frame (channel identity is assumed always 0)
+    iplane = 0
+    # loop over all tiffs
+    for file in fs:
+        im = io.imread(file)
+        nframes = im.shape[0]
+        for j in range(0,nplanes):    
+            i0 = nchannels * ((iplane+j)%nplanes)
+            im2write = im[np.arange(int(i0), nframes, nplanes*nchannels),:,:]
+            reg_file[j].write(bytearray(im2write))
+            if nchannels>1:
+                im2write = im[np.arange(i0+1, nframes, nplanes*nchannels),:,:]
+                reg_file_chan2[j].write(bytearray(im2write))            
+        iplane = (iplane+nframes/nchannels)%nplanes    
+    # write ops files
+    for ops in ops1:        
+        ops['Ly'] = im.shape[1]
+        ops['Lx'] = im.shape[2]        
+        np.save(ops['ops_path'], ops)    
+    # close all binary files and write ops files
+    for j in range(0,nplanes):
+        reg_file[j].close()        
+        if nchannels>1:
+            reg_file_chan2[j].close()
+    return ops1
+
+def list_tifs(ops, froot):      
+    lpath = os.path.join(froot, "*.tif")
+    fs  = sorted(glob.glob(lpath)) 
+    lpath = os.path.join(froot, "*.tiff")
+    fs2 = sorted(glob.glob(lpath))
+    fs.extend(fs2)    
+    if ops['look_one_level_down']:
+        fdir = glob.glob(os.path.join(froot, "*", ""))
+        for folder_down in fdir:            
+            lpath = os.path.join(froot, folder_down, "*.tif")
+            fs3 = sorted(glob.glob(lpath))
+            lpath = os.path.join(froot, folder_down, "*.tiff")
+            fs4 = sorted(glob.glob(lpath))
+            fs.extend(fs3)
+            fs.extend(fs4)            
+    return fs
+
+def get_tif_list(ops):
+    froot = ops['data_path']
+    if len(ops['subfolders'])==0:        
+        fs = list_tifs(ops, froot)
+    else:
+        fs = []
+        for folder_down in ops['subfolders']:
+            fold = os.path.join(froot, folder_down)
+            fs.extend(list_tifs(ops, fold))            
+    if fs is None:
+        raise Exception('Could not find any tifs')
+    return fs
---
+++ register2.py
@@ -0,0 +1,190 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from numpy import fft
+from numpy import random as rnd
+from joblib import Parallel, delayed
+import multiprocessing
+import time
+
+eps0 = 1e-5;
+sigL = 0.85 # smoothing width for up-sampling kernels, keep it between 0.5 and 1.0...
+lpad = 3   # upsample from a square +/- lpad 
+smoothSigma = 1.15 # smoothing constant
+maskSlope   = 2. # slope of taper mask at the edges
+    
+# smoothing kernel
+def kernelD(a, b):        
+    dxs = np.reshape(a[0], (-1,1)) - np.reshape(b[0], (1,-1))
+    dys = np.reshape(a[1], (-1,1)) - np.reshape(b[1], (1,-1))
+    
+    ds = np.square(dxs) + np.square(dys)
+    K = np.exp(-ds/(2*np.square(sigL)))    
+    return K
+    
+def mat_upsample(lpad, ops):    
+    lar    = np.arange(-lpad, lpad+1)
+    larUP  = np.arange(-lpad, lpad+.001, 1./ops['subpixel'])
+
+    x, y   = np.meshgrid(lar, lar)   
+    xU, yU = np.meshgrid(larUP, larUP)   
+
+    Kx = kernelD((x,y),(x,y))
+    Kx = np.linalg.inv(Kx)
+    Kg = kernelD((x,y),(xU,yU))
+    
+    Kmat = np.dot(Kx, Kg)
+    nup = larUP.shape[0]
+    
+    return Kmat, nup
+
+
+def prepareMasks(refImg):    
+    i0,Ly,Lx = refImg.shape
+    
+    x = np.arange(0, Lx)
+    y = np.arange(0, Ly)
+    x = np.abs(x - x.mean())
+    y = np.abs(y - y.mean())
+
+    xx, yy = np.meshgrid(x, y)
+
+    mY = y.max() - 4.
+    mX = x.max() - 4.
+    
+    maskY = 1./(1.+np.exp((yy-mY)/maskSlope))
+    maskX = 1./(1.+np.exp((xx-mX)/maskSlope))
+    maskMul = maskY * maskX
+    maskOffset = refImg.mean() * (1. - maskMul);
+
+    #plt.imshow(maskMul)
+    #plt.show()
+    #plt.imshow(maskOffset)
+    #plt.show()
+    
+    hgx = np.exp(-np.square(xx/smoothSigma))
+    hgy = np.exp(-np.square(yy/smoothSigma))
+    hgg = hgy * hgx
+    hgg = hgg/hgg.sum()
+
+    fhg = np.real(fft.fft2(fft.ifftshift(hgg))); # smoothing filter in Fourier domain
+
+    cfRefImg   = np.conj(fft.fft2(refImg));
+    absRef     = np.absolute(cfRefImg);
+    cfRefImg   = cfRefImg / (eps0 + absRef) * fhg;
+    
+    return maskMul, maskOffset, cfRefImg
+
+def correlation_map(data, cfRefImg):
+    X = fft.fft2(data)
+    J = X / (eps0 + np.absolute(X)) 
+    J = J * cfRefImg
+    cc = np.real(fft.ifft2(J))
+    cc = fft.fftshift(cc, axes=(1,2))
+    return cc, X
+
+def getXYup(cc, Ls, ops):
+    (lcorr, lpad, Lyhalf, Lxhalf) = Ls
+    
+    nimg = cc.shape[0]
+    cc0 = cc[:, (Lyhalf-lcorr):(Lyhalf+lcorr+1), (Lxhalf-lcorr):(Lxhalf+lcorr+1)]
+
+    cc0 = np.reshape(cc0, (nimg, -1))
+    ix  = np.argmax(cc0, axis = 1)
+
+    ymax, xmax = np.unravel_index(ix, (2*lcorr+1,2*lcorr+1))
+    mxpt = [ymax+Lyhalf-lcorr, xmax + Lxhalf-lcorr]
+    
+    ccmat = np.zeros((nimg, 2*lpad+1, 2*lpad+1))
+    for j in range(0, nimg):
+        ccmat[j,:,:] = cc[j, (mxpt[0][j] -lpad):(mxpt[0][j] +lpad+1), (mxpt[1][j] -lpad):(mxpt[1][j] +lpad+1)]
+
+    ccmat = np.reshape(ccmat, (nimg,-1))
+
+    Kmat, nup = mat_upsample(lpad, ops)
+
+    ccb = np.dot(ccmat, Kmat)
+    imax = np.argmax(ccb, axis=1)
+    cmax = np.amax(ccb, axis=1)
+
+    ymax, xmax = np.unravel_index(imax, (nup,nup))
+
+    mdpt = np.floor(nup/2)
+    
+    ymax,xmax = (ymax-mdpt)/ops['subpixel'], (xmax-mdpt)/ops['subpixel']
+    
+    ymax, xmax = ymax + mxpt[0] - Lyhalf, xmax + mxpt[1] - Lxhalf
+
+    return ymax, xmax
+
+def shift_data(X, ymax,xmax):
+    nimg, Ly, Lx = X.shape
+    
+    Ny = fft.ifftshift(np.arange(-np.fix(Ly/2), np.ceil(Ly/2)))
+    Nx = fft.ifftshift(np.arange(-np.fix(Lx/2), np.ceil(Lx/2)))
+    [Nx,Ny] = np.meshgrid(Nx,Ny)
+    Nx = Nx / Lx
+    Ny = Ny / Ly
+    
+    dph = Nx * np.reshape(xmax, (-1,1,1)) + Ny * np.reshape(ymax, (-1,1,1))
+    
+    Y = np.real(fft.ifft2(X * np.exp((2j * np.pi) * dph)))
+    return Y
+    
+def phasecorr_worker(data, refImg, ops):   
+    
+    nimg, Ly, Lx = data.shape
+    refImg = np.reshape(refImg, (1, Ly, Lx))
+    
+    Lyhalf = int(np.floor(Ly/2))
+    Lxhalf = int(np.floor(Lx/2))
+    
+    tstart = time.time()
+    
+    if ops['maxregshift']>0:
+        maxregshift = ops['maxregshift'] 
+    else:
+        maxregshift = np.round(.1*np.maximum(Ly, Lx))
+
+    lcorr = int(np.minimum(maxregshift, np.floor(np.minimum(Ly,Lx)/2.)-lpad))
+
+    (maskMul, maskOffset, cfRefImg) = prepareMasks(refImg)
+
+    data = data * maskMul + maskOffset
+
+    cc, X = correlation_map(data, cfRefImg)
+
+    ymax, xmax = getXYup(cc, (lcorr,lpad, Lyhalf, Lxhalf), ops)
+    
+    Y = shift_data(X, ymax,xmax)
+    
+    return Y, ymax, xmax, tstart
+
+def phasecorr(data, refImg, ops, pool):    
+    nimg = data.shape[0]
+    
+    num_cores = 20
+    nbatch = int(np.ceil(nimg/float(num_cores)))
+    
+    inputs = range(0, nimg, nbatch)
+    irange = []
+    for i in inputs:
+        irange.append(i + np.arange(0,np.minimum(nbatch, nimg-i)))
+
+    t0 = time.time()
+    
+    results = [pool.apply_async(phasecorr_worker(data[irange[j],:, :], refImg, ops) for j in range(0,len(irange)))]
+    #results = Parallel(n_jobs=num_cores)(delayed(phasecorr_worker)(data[irange[j],:, :], refImg, ops) for j in range(0,len(irange)))
+
+    Y = np.zeros_like(data)
+    ymax = np.zeros((nimg,))
+    xmax = np.zeros((nimg,))
+    tout = np.zeros((len(results),))
+    for i in range(0,len(results)):
+        Y[irange[i], :, :] = results[i][0]
+        ymax[irange[i]] = results[i][1]
+        xmax[irange[i]] = results[i][2]
+        tout[i] = results[i][3] - t0
+
+    return Y, ymax, xmax, tout
+

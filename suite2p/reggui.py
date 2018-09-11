cdd307569b3632d19e1b562b80c0479dd41a1bbb
carsen-stringer <carsen.stringer@gmail.com>
2018-09-11 11:07:19 -0400

    bugs in registration gui

--- suite2p/reggui.py
+++ suite2p/reggui.py
@@ -1,7 +1,7 @@
 # heavily modified script from a pyqt4 release
 
 from PyQt5 import QtGui, QtCore
-from suite2p import register
+from suite2p import register,fig
 import pyqtgraph as pg
 import os
 import sys
@@ -11,7 +11,7 @@ class BinaryPlayer(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(BinaryPlayer, self).__init__(parent)
         pg.setConfigOptions(imageAxisOrder='row-major')
-        self.setGeometry(70,70,1100,900)
+        self.setGeometry(70,70,1070,1070)
         self.setWindowTitle('View registered binary')
         self.cwidget = QtGui.QWidget(self)
         self.setCentralWidget(self.cwidget)
@@ -32,11 +32,11 @@ class BinaryPlayer(QtGui.QMainWindow):
         self.p0.setMenuEnabled(False)
         self.pimg = pg.ImageItem()
         self.p0.addItem(self.pimg)
-        self.p1 = self.win.addPlot(row=1,col=0)
+        self.p1 = self.win.addPlot(name='plot1',row=1,col=0)
         self.p1.setMouseEnabled(x=True,y=False)
         self.p1.setMenuEnabled(False)
         #self.p1.autoRange(padding=0.01)
-        self.p2 = self.win.addPlot(row=2,col=0)
+        self.p2 = self.win.addPlot(name='plot2',row=2,col=0)
         self.p2.setMouseEnabled(x=True,y=False)
         self.p2.setMenuEnabled(False)
         #self.p2.autoRange(padding=0.01)
@@ -44,10 +44,21 @@ class BinaryPlayer(QtGui.QMainWindow):
         self.movieLabel = QtGui.QLabel("No ops chosen")
         self.movieLabel.setStyleSheet("color: white;")
         self.movieLabel.setAlignment(QtCore.Qt.AlignCenter)
-        #self.movieLabel.setSizePolicy(QtGui.QSizePolicy.Ignored, QtGui.QSizePolicy.Ignored)
         self.nframes = 0
         self.cframe = 0
         self.createButtons()
+        # create ROI chooser
+        self.l0.addWidget(QtGui.QLabel(''),2,0,1,2)
+        qlabel = QtGui.QLabel(self)
+        qlabel.setText("<font color='white'>Selected ROI:</font>")
+        self.l0.addWidget(qlabel,3,0,1,2)
+        self.ROIedit = QtGui.QLineEdit(self)
+        self.ROIedit.setValidator(QtGui.QIntValidator(0,10000))
+        self.ROIedit.setText('0')
+        self.ROIedit.setFixedWidth(45)
+        self.ROIedit.setAlignment(QtCore.Qt.AlignRight)
+        self.ROIedit.returnPressed.connect(self.number_chosen)
+        self.l0.addWidget(self.ROIedit, 4,0,1,1)
         # create frame slider
         self.frameLabel = QtGui.QLabel("Current frame:")
         self.frameLabel.setStyleSheet("color: white;")
@@ -67,26 +78,23 @@ class BinaryPlayer(QtGui.QMainWindow):
         #self.speedSpinBox.setRange(1, 9999)
         #self.speedSpinBox.setValue(100)
         #self.speedSpinBox.setSuffix("%")
-        #self.controlsLayout.addWidget(speedLabel, 2, 0)
-        #self.controlsLayout.addWidget(self.speedSpinBox, 2, 1)
-
-        #self.movie.frameChanged.connect(self.updateFrameSlider)
-        #self.movie.stateChanged.connect(self.updateButtons)
-        #self.fitCheckBox.clicked.connect(self.fitToWindow)
         self.frameSlider.valueChanged.connect(self.go_to_frame)
-        #self.speedSpinBox.valueChanged.connect(self.movie.setSpeed)
         self.l0.addWidget(self.movieLabel,0,0,1,5)
         self.updateFrameSlider()
         self.updateButtons()
         self.updateTimer = QtCore.QTimer()
         self.updateTimer.timeout.connect(self.next_frame)
         self.cframe = 0
+        self.loaded = False
+        self.win.scene().sigMouseClicked.connect(self.plot_clicked)
         # if not a combined recording, automatically open binary
         if hasattr(parent, 'ops'):
             if parent.ops['save_path'][-8:]!='combined':
                 fileName = os.path.join(parent.basename, 'ops.npy')
                 print(fileName)
-                self.openFile(fileName)
+                self.Fcell = parent.Fcell
+                self.stat = parent.stat
+                self.openFile(fileName, True)
 
     def open(self):
         fileName = QtGui.QFileDialog.getOpenFileName(self,
@@ -94,9 +102,9 @@ class BinaryPlayer(QtGui.QMainWindow):
         # load ops in same folder
         if fileName:
             print(fileName[0])
-            self.openFile(fileName[0])
+            self.openFile(fileName[0], False)
 
-    def openFile(self, fileName):
+    def openFile(self, fileName, fromgui):
         try:
             ops = np.load(fileName)
             ops = ops.item()
@@ -105,49 +113,128 @@ class BinaryPlayer(QtGui.QMainWindow):
             self.reg_loc = ops['reg_file']
             self.reg_file = open(self.reg_loc,'rb')
             self.reg_file.close()
+            if not fromgui:
+                self.Fcell = np.load(os.path.join(os.path.dirname(fileName),'F.npy'))
+                self.stat =  np.load(os.path.join(os.path.dirname(fileName),'stat.npy'))
+                ncells = len(self.stat)
+                for n in range(0,ncells):
+                    ypix = self.stat[n]['ypix'].flatten()
+                    xpix = self.stat[n]['xpix'].flatten()
+                    iext = fig.boundary(ypix,xpix)
+                    self.stat[n]['yext'] = ypix[iext]
+                    self.stat[n]['xext'] = xpix[iext]
+                    ycirc,xcirc = fig.circle(self.stat[n]['med'],self.stat[n]['radius'])
+                    goodi = (ycirc>=0) & (xcirc>=0) & (ycirc<self.Ly) & (xcirc<self.Lx)
+                    self.stat[n]['ycirc'] = ycirc[goodi]
+                    self.stat[n]['xcirc'] = xcirc[goodi]
             good = True
-        except (ValueError, OSError, RuntimeError, TypeError, NameError):
-            print("ERROR: ops.npy incorrect / missing ops['reg_file']")
+        except Exception as e:
+            print("ERROR: ops.npy incorrect / missing ops['reg_file'] and others")
+            print(e)
             good = False
         if good:
             self.p1.clear()
             self.p2.clear()
+            self.ichosen = 0
+            self.ROIedit.setText('0')
             # get scaling from 100 random frames
             frames = register.subsample_frames(ops, np.minimum(ops['nframes'],100))
             self.srange = frames.mean() + frames.std()*np.array([-3,3])
+            self.cell_mask()
             #self.srange = [np.percentile(frames.flatten(),8), np.percentile(frames.flatten(),99)]
             self.reg_file = open(self.reg_loc,'rb')
             self.movieLabel.setText(self.reg_loc)
-            self.cframe = 0
             self.nbytesread = 2 * self.Ly * self.Lx
-            self.currentMovieDirectory = QtCore.QFileInfo(fileName).path()
-            buff = self.reg_file.read(self.nbytesread)
-            self.img = np.reshape(np.frombuffer(buff, dtype=np.int16, offset=0),(self.Ly,self.Lx))
-            self.pimg.setImage(self.img)
-            self.pimg.setLevels(self.srange)
             self.nframes = ops['nframes']
+            self.currentMovieDirectory = QtCore.QFileInfo(fileName).path()
             if self.nframes > 0:
                 self.updateFrameSlider()
                 self.updateButtons()
             # plot ops X-Y offsets
             self.yoff = ops['yoff']
             self.xoff = ops['xoff']
-            self.p1.plot(self.yoff)
-            self.p1.plot(self.xoff)
+            self.p1.plot(self.yoff, pen='g')
+            self.p1.plot(self.xoff, pen='y')
             self.p1.setRange(xRange=(0,self.nframes),
-                             yRange=(self.yoff.min(),self.yoff.max()),
+                             yRange=(np.minimum(self.yoff.min(),self.xoff.min()),
+                                     np.maximum(self.yoff.max(),self.xoff.max())),
                              padding=0.0)
+            self.p1.setLimits(xMin=0,xMax=self.nframes)
             self.scatter1 = pg.ScatterPlotItem()
             self.p1.addItem(self.scatter1)
-            self.p2.setRange(xRange=(0,self.nframes),
-                             yRange=(self.xoff.min(),self.xoff.max()),
-                             padding=0.0)
-            #self.scatter2 = pg.ScatterPlotItem()
-            #self.p2.addItem(self.scatter2)
             self.scatter1.setData([self.cframe,self.cframe],
                                   [self.yoff[self.cframe],self.xoff[self.cframe]],
                                   size=10,brush=pg.mkBrush(255,0,0))
-            #self.scatter2.setData([self.cframe],[self.xoff[self.cframe,0]],size=10,brush=pg.mkBrush(255,0,0))
+            self.ft = self.Fcell[self.ichosen,:]
+            self.p2.plot(self.ft, pen='b')
+            self.p2.setRange(xRange=(0,self.nframes),
+                             yRange=(self.ft.min(),self.ft.max()),
+                             padding=0.0)
+            self.p2.setLimits(xMin=0,xMax=self.nframes)
+            self.scatter2 = pg.ScatterPlotItem()
+            self.p2.addItem(self.scatter2)
+            self.scatter2.setData([self.cframe],[self.ft[self.cframe]],size=10,brush=pg.mkBrush(255,0,0))
+            self.p2.setXLink('plot1')
+            self.cframe = -1
+            self.loaded = True
+            self.next_frame()
+
+    def number_chosen(self):
+        if self.loaded:
+            self.ichosen = int(self.ROIedit.text())
+            if self.ichosen >= len(self.stat):
+                self.ichosen = len(self.stat) - 1
+            self.cell_mask()
+            self.p2.clear()
+            self.ft = self.Fcell[self.ichosen,:]
+            self.p2.plot(self.ft,pen='b')
+            self.p2.setRange(yRange=(self.ft.min(),self.ft.max()))
+            self.scatter2 = pg.ScatterPlotItem()
+            self.p2.addItem(self.scatter2)
+            self.scatter2.setData([self.cframe],[self.ft[self.cframe]],size=10,brush=pg.mkBrush(255,0,0))
+            self.p2.setXLink('plot1')
+            self.jump_to_frame(self.cframe)
+            self.show()
+
+    def plot_clicked(self,event):
+        items = self.win.scene().items(event.scenePos())
+        posx  = 0
+        posy  = 0
+        iplot = 0
+        zoom = False
+        choose = False
+        if self.loaded:
+            for x in items:
+                if x==self.p1:
+                    vb = self.p1.vb
+                    pos = vb.mapSceneToView(event.scenePos())
+                    posx = pos.x()
+                    iplot = 1
+                elif x==self.p2:
+                    vb = self.p1.vb
+                    pos = vb.mapSceneToView(event.scenePos())
+                    posx = pos.x()
+                    iplot = 2
+                if iplot==1 or iplot==2:
+                    if event.button()==1:
+                        if event.double():
+                            zoom=True
+                        else:
+                            choose=True
+        if zoom:
+            self.p1.setRange(xRange=(0,self.nframes))
+        if choose:
+            if self.playButton.isEnabled():
+                self.cframe = np.maximum(0, np.minimum(self.nframes-1, int(np.round(posx))))
+                self.frameSlider.setValue(self.cframe)
+                self.jump_to_frame(self.cframe)
+
+
+    def cell_mask(self):
+        self.cmask = np.zeros((self.Ly,self.Lx,3),np.float32)
+        self.yext = self.stat[self.ichosen]['yext']
+        self.xext = self.stat[self.ichosen]['xext']
+        self.cmask[self.yext,self.xext,2] = (self.srange[1]-self.srange[0])/2 * np.ones((self.yext.size,),np.float32)
 
     def go_to_frame(self, frame):
         self.jump_to_frame(frame)
@@ -218,7 +305,11 @@ class BinaryPlayer(QtGui.QMainWindow):
     def next_frame(self):
         if self.cframe < self.nframes - 1:
             buff = self.reg_file.read(self.nbytesread)
-            self.img = np.reshape(np.frombuffer(buff, dtype=np.int16, offset=0),(self.Ly,self.Lx))
+            self.img = np.reshape(np.frombuffer(buff, dtype=np.int16, offset=0),(self.Ly,self.Lx))[:,:,np.newaxis]
+            self.img = np.tile(self.img,(1,1,3))
+            self.img[self.yext,self.xext,0] = self.srange[0]
+            self.img[self.yext,self.xext,1] = self.srange[0]
+            self.img[self.yext,self.xext,2] = (self.srange[1]) * np.ones((self.yext.size,),np.float32)
             self.pimg.setImage(self.img)
             self.pimg.setLevels(self.srange)
             self.cframe+=1
@@ -227,13 +318,17 @@ class BinaryPlayer(QtGui.QMainWindow):
             self.scatter1.setData([self.cframe,self.cframe],
                                   [self.yoff[self.cframe],self.xoff[self.cframe]],
                                   size=10,brush=pg.mkBrush(255,0,0))
+            self.scatter2.setData([self.cframe],[self.ft[self.cframe]],size=10,brush=pg.mkBrush(255,0,0))
+        else:
+            self.pauseButton.setChecked(True)
+            self.pause()
 
     def start(self):
         if self.cframe < self.nframes - 1:
             print('playing')
             self.playButton.setEnabled(False)
             self.pauseButton.setEnabled(True)
-            self.updateTimer.start(25)
+            self.updateTimer.start(30)
 
     def pause(self):
         self.updateTimer.stop()

e1fe95b3bc1aa5091524f2d86b70f74628f40da5
carsen-stringer <carsen.stringer@gmail.com>
2018-09-20 10:16:02 -0400

    added nonrigid registration options to run gui

--- suite2p/run_s2p.py
+++ suite2p/run_s2p.py
@@ -40,23 +40,28 @@ def default_ops():
         'num_workers_roi': -1, # 0 to select number of planes, -1 to disable parallelism, N to enforce value
         # registration settings
         'do_registration': True, # whether to register data
-        'nonrigid': False, # whether to use nonrigid registration
         'nimg_init': 200, # subsampled frames for finding reference image
         'batch_size': 200, # number of frames per batch
         'maxregshift': 0.1, # max allowed registration shift, as a fraction of frame max(width and height)
         'align_by_chan' : 1, # when multi-channel, you can align by non-functional channel (1-based)
         'reg_tif': False, # whether to save registered tiffs
         'subpixel' : 10, # precision of subpixel registration (1/subpixel steps)
+        # nonrigid registration options
+        'nonrigid': False, # whether to use nonrigid registration
+        'block_size': [128,128], # block size in number of pixels in Y and X (2 numbers separated by a ,)
+        'snr_thresh': 1., # if any nonrigid block is below this threshold, it gets smoothed until above this threshold. 1.0 results in no smoothing
+        'maxregshiftNR': 5, # maximum pixel shift allowed for nonrigid, relative to rigid
         # cell detection settings
         'connected': True, # whether or not to keep ROIs fully connected (set to 0 for dendrites)
         'navg_frames_svd': 5000, # max number of binned frames for the SVD
         'nsvd_for_roi': 1000, # max number of SVD components to keep for ROI detection
         'max_iterations': 20, # maximum number of iterations to do cell detection
+        'smooth_masks': 1, # whether to smooth masks in the final pass of cell detection
+        'threshold_scaling': 1., # adjust the automatically determined threshold by this scalar multiplier
+        'max_overlap': 0.75, # cells with more overlap than this get removed during triage, before refinement
         'ratio_neuropil': 6., # ratio between neuropil basis size and cell radius
         'ratio_neuropil_to_cell': 3, # minimum ratio between neuropil radius and cell radius
         'tile_factor': 1., # use finer (>1) or coarser (<1) tiles for neuropil estimation during cell detection
-        'threshold_scaling': 1., # adjust the automatically determined threshold by this scalar multiplier
-        'max_overlap': 0.75, # cells with more overlap than this get removed during triage, before refinement
         'inner_neuropil_radius': 2, # number of pixels to keep between ROI and neuropil donut
         'outer_neuropil_radius': np.inf, # maximum neuropil radius
         'min_neuropil_pixels': 350, # minimum number of pixels in the neuropil
@@ -69,9 +74,7 @@ def default_ops():
         'allow_overlap': False,
         'xrange': np.array([0, 0]),
         'yrange': np.array([0, 0]),
-        'smooth_masks': 1, # whether to smooth masks in the final pass of cell detection
-        'snr_thresh': 1., # if any nonrigid block is below this threshold, it gets smoothed until above this threshold. 1.0 results in no smoothing
-        'maxregshiftNR': 5, # maximum pixel shift allowed for nonrigid, relative to rigid        
+
       }
     return ops
 
@@ -111,7 +114,7 @@ def run_s2p(ops={},db={}):
     else:
         files_found_flag = False
         flag_binreg = False
-    print(flag_binreg)
+    #print(flag_binreg)
     ######### REGISTRATION #########
     if not files_found_flag:
         # get default options

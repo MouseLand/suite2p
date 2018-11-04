271585e0c539ad127d42a1c1dc038d914ab40d64
Carsen Stringer <carsen.stringer@gmail.com>
2018-11-04 16:05:50 -0500

    commented out autoload

--- suite2p/gui2p.py
+++ suite2p/gui2p.py
@@ -512,8 +512,8 @@ class MainW(QtGui.QMainWindow):
         model = model.item()
         self.default_keys = model["keys"]
         # self.fname = '/media/carsen/DATA2/Github/TX4/stat.npy'
-        self.fname = 'C:/Users/carse/github/TX4/stat.npy'
-        self.load_proc()
+        #self.fname = 'C:/Users/carse/github/TX4/stat.npy'
+        #self.load_proc()
         # self.load_behavior('C:/Users/carse/github/TX4/beh.npy')
 
     def export_fig(self):

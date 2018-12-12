0682a8a325fdb309617e6778ee351cd34af309a4
carsen-stringer <carsen.stringer@gmail.com>
2018-12-12 17:09:40 -0500

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="suite2p",
-    version="0.4.6",
+    version="0.4.7",
     author="Marius Pachitariu and Carsen Stringer",
     author_email="marius10p@gmail.com",
     description="Pipeline for calcium imaging",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MouseLand/suite2p",
     packages=setuptools.find_packages(),
-    install_requires = ['pyqtgraph', 'PyQt5', 'numpy>=1.13.0', 'scipy', 'h5py', 'scikit-image', 'scikit-learn', 'matplotlib','rastermap'],
+    install_requires = ['pyqtgraph', 'PyQt5', 'numpy>=1.13.0', 'scipy', 'h5py', 'natsort', 'scikit-image', 'scikit-learn', 'matplotlib','rastermap'],
     include_package_data=True,
     classifiers=(
         "Programming Language :: Python :: 3",

3f907640521945a2dc7588057d2c6621724a69b4
Emerson Harkin <emerson.f.harkin@gmail.com>
2019-07-11 17:52:01 -0400

    Fix missing indent error in LoadBinary

--- suite2p/LoadBinary.py
+++ suite2p/LoadBinary.py
@@ -5,7 +5,8 @@ def binMovie(ops):
     mov = np.zeros((ops['Ly'], ops['Lx'], ops['navg_frames_svd']), np.float32)
 
     with open(ops['reg_file'], 'r') as binary_file:
-    # Read the whole file at once
-    data = binary_file.read()
-    print(data)
+        # Read the whole file at once
+        data = binary_file.read()
+        print(data)
+
     return mov

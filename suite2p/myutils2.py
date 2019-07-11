a5b5aab90bea7a7bebd5823e4d0181416824c05a
Emerson Harkin <emerson.f.harkin@gmail.com>
2019-07-11 17:52:34 -0400

    Fix missing time import in myutils2

--- suite2p/myutils2.py
+++ suite2p/myutils2.py
@@ -1,20 +1,21 @@
+import time
 import numpy as np
 from scipy import signal
 
 def conv2(s1, sig, axes=None):
     if axes is None:
         axes = np.arange(0,len(sig))
-    
+
     axes = np.array(axes)
     if isinstance(sig, (int, float)) or len(sig)==1:
         sig = sig*np.ones((len(axes),1),np.float32)
     elif len(sig)>1 and len(sig) != len(axes):
         raise ValueError('number of axes different from number of smoothing constants')
-    
+
     s1 = np.array(s1).astype(np.float32)
     sdim = s1.ndim
     sig = np.array(sig).astype(np.float32)
-    
+
     sfilt = s1
     for i in range(0,len(axes)):
         dims = np.arange(-1,sdim)
@@ -23,12 +24,12 @@ def conv2(s1, sig, axes=None):
         sfilt = np.transpose(sfilt, dims)
         print(dims)
         ns = sfilt.shape[0]
-        
+
         tmax = np.ceil(4*sig[i])
         dt = np.arange(-tmax,tmax)
         gaus = np.exp(-dt**2 / (2*sig[i]**2))
         gaus /= gaus.sum()
-        
+
         flat = np.ones((ns,),np.float32)
         snorm = signal.convolve(flat, gaus, mode='same')
         for j in range(0,sdim-1):
@@ -39,7 +40,7 @@ def conv2(s1, sig, axes=None):
         t0 = time.time()
         sfilt = signal.convolve(sfilt, gaus, mode='same')
         print(time.time()-t0)
-        
+
         #if sfilt.shape[0] > ns:
         #    icent = np.floor(sfilt.shape[0]/2) - np.floor(ns/2)
         #    inds  = (icent + np.arange(0,ns)).astype(np.int32)
@@ -56,6 +57,5 @@ def conv2(s1, sig, axes=None):
 def conv_circ(s1, sig, axes=None):
     if axes is None:
         axes = np.arange(0,1)
-        
+
     return s1
-    

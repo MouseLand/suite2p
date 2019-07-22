9ef777ec6e9bc6e93f2024ac5ef937e55326126e
neuraldata <carsen.stringer@gmail.com>
2019-07-22 08:55:02 -0400

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="suite2p",
-    version="0.6.11",
+    version="0.6.12",
     author="Marius Pachitariu and Carsen Stringer",
     author_email="marius10p@gmail.com",
     description="Pipeline for calcium imaging",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/MouseLand/suite2p",
     packages=setuptools.find_packages(),
     install_requires = ['pyqtgraph', 'PyQt5', 'numpy>=1.13.0', 'scipy', 'h5py', 'natsort','mkl_fft',
-                        'numba','scikit-image', 'scikit-learn', 'matplotlib','rastermap==0.1.0',
+                        'numba','scikit-image', 'scikit-learn', 'matplotlib','rastermap>=0.1.0',
                         'scanimage-tiff-reader'],
     include_package_data=True,
     classifiers=(

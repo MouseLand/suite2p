ec27131ab1f771abdf7b26c01124aa0aecd77ac6
flora <maria.kesa@gmail.com>
2019-10-02 23:53:59 -0400

    Add clickable plots

--- suite2p/gui/ensemble_pursuit.py
+++ suite2p/gui/ensemble_pursuit.py
@@ -10,6 +10,9 @@ import time
 import sys,os
 from rastermap.mapping import Rastermap
 from . import rungui,masks
+import sys
+sys.path.append("..")
+from EnsemblePursuit.EnsemblePursuitModule.EnsemblePursuit import EnsemblePursuit
 
 class EPWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -31,35 +34,60 @@ class EPWindow(QtGui.QMainWindow):
         self.l0.addWidget(self.win,0,0,14,14)
         layout = self.win.ci.layout
 
-        self.sp=parent.Fbin
+        self.sp=parent.Fbin.T
 
         self.all_v = self.win.addPlot(title='ZOOM IN',row=1,col=0,colspan=2)
         self.imgROI = pg.ImageItem(autoDownsample=True)
         #self.p2.addItem(self.imgROI)
-        self.all_v.setMouseEnabled(x=False,y=False)
+        self.all_v.setMouseEnabled(x=True,y=True)
         #self.p2.setLabel('left', 'neurons')
-        self.all_v.hideAxis('bottom')
 
+        self.epOn = QtGui.QPushButton('compute EnsemblePursuit')
+        self.epOn.clicked.connect(lambda: self.compute_ep(parent))
+        self.l0.addWidget(self.epOn,0,0,1,2)
 
     def compute_ep(self, parent):
-        ops = {'n_components': 3, 'lambda': 0.01}
-        options_dict={'seed_neuron_av_nr':100,'min_assembly_size':8}
-        ep_np=EnsemblePursuitNumpyFast(n_ensembles=1,lambd=0.01,options_dict=options_dict)
+        ops = {'n_components': 25, 'lam': 0.01}
+        self.n_components=ops['n_components']
         self.error=False
         self.finish=True
-        self.mapOn.setEnabled(False)
+        self.epOn.setEnabled(False)
         self.tic=time.time()
-        try:
-            self.model = EnsemblePursuitNumpyFast(n_components=ops['n_components'],lambd=ops['lambda'],options_dict=options_dict )
-            self.U,self.V=self.model.fit_transform_kmeans_init(self.sp)
+        #try:
+        self.model = EnsemblePursuit(n_components=ops['n_components'],lam=ops['lam'])
+        self.U,self.V=self.model.fit(self.sp)
             #proc  = {'embedding': model.embedding, 'uv': [model.u, model.v],
             #         'ops': ops, 'filename': args.S, 'train_time': train_time}
             #basename, fname = os.path.split(args.S)
             #np.save(os.path.join(basename, 'embedding.npy'), proc)
-            print('ep computed in %3.2f s'%(time.time()-self.tic))
-            self.activate(parent)
-        except:
-            print('ep issue: Interrupted by error (not finished)\n')
+        print('ep computed in %3.2f s'%(time.time()-self.tic))
+            #self.activate(parent)
+        self.plot_v()
+        #except:
+            #print('ep issue: Interrupted by error (not finished)\n')
         #self.process.start('python -u -W ignore -m rastermap --S %s --ops %s'%
         #                    (spath, opspath))
         print(self.V,self.V.shape)
+
+    def plot_v(self):
+        #self.all_v.plot(self.V.T)
+        self.all_v.setYRange(0, self.V.shape[1], padding=0)
+        self.linep=[]
+        for j in range(self.V.shape[1]):
+            self.linep.append(self.all_v.plot(self.V.T[j,:]+j, clickable=True))
+        self.win.scene().sigMouseClicked.connect(self.plot_clicked)
+        self.show()
+        self.win.show()
+
+    def plot_clicked(self,event):
+        vb=self.all_v.vb
+        pos = vb.mapSceneToView(event.scenePos()).y()
+        ranges=np.arange(0,self.V.shape[1]+1)
+        print(ranges)
+        print('pos',pos)
+        for j in range(0,ranges.shape[0]-1):
+            if ranges[j]<=pos<=ranges[j+1]:
+                print(j)
+                self.linep[j]=self.all_v.plot(self.V.T[j,:]+j, clickable=True,pen='b')
+        self.show()
+        self.win.show()

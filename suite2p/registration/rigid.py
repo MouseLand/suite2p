ba4fea2e31df87b284f6d4e57718090512ea5b16
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-05-22 16:29:47 +0200

    changed mkl_fft importerror to warning to support rtd docs

--- suite2p/registration/rigid.py
+++ suite2p/registration/rigid.py
@@ -5,7 +5,10 @@ from numpy import fft
 from numba import vectorize, complex64, float32, int16
 import math
 from scipy.ndimage import gaussian_filter1d
-from mkl_fft import fft2, ifft2
+try:
+    from mkl_fft import fft2, ifft2
+except ImportError:
+    raise ImportWarning("mkl_fft not installed.  Install it with conda: conda install mkl_fft")
 from . import utils
 
 def phasecorr_reference(refImg0, ops):
--- suite2p/registration/utils.py
+++ suite2p/registration/utils.py
@@ -4,7 +4,10 @@ from numpy import fft
 from numba import vectorize, complex64, float32, int16
 import math
 from scipy.ndimage import gaussian_filter1d
-from mkl_fft import fft2
+try:
+    from mkl_fft import fft2, ifft2
+except ImportError:
+    raise ImportWarning("mkl_fft not installed.  Install it with conda: conda install mkl_fft")
 
 def one_photon_preprocess(data, ops):
     ''' pre filtering for one-photon data '''

d25b9b53e57e704d91f2b1edd70961581b0e9de7
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-05-25 00:02:26 +0200

    simplified subpackage inits

--- suite2p/classification/__init__.py
+++ suite2p/classification/__init__.py
@@ -1,2 +1,1 @@
-from . import classifier
 from .classifier import Classifier
--- suite2p/gui/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import (buttons, classgui, drawroi, graphics, io, masks, gui2p, menus,
-                merge, reggui, rungui, traces, views, visualize)

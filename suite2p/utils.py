6ba26940499ef5d0f5f9463e010384b9a81a3e2a
Chris <chriski777@berkeley.edu>
2020-05-23 19:54:07 -0700

    Removed unnecessary imports. Added download_url_to_file function from cellpose.

--- suite2p/utils.py
+++ suite2p/utils.py
@@ -3,10 +3,12 @@ import numpy as np
 from natsort import natsorted
 import math, time
 import glob, h5py, os, json
-from scipy import signal
 from scipy import stats, signal
-from scipy.sparse import linalg
-import scipy.io
+from tqdm import tqdm
+from urllib.request import urlopen
+import tempfile
+import shutil
+
 
 def boundary(ypix,xpix):
     """ returns pixels of mask that are on the exterior of the mask """
@@ -177,3 +179,42 @@ def get_frames(ops, ix, bin_file, crop=False, badframes=False):
             else:
                 mov[i,:,:] = data
     return mov
+
+
+def download_url_to_file(url, dst, progress=True):
+    r"""Download object at the given URL to a local path.
+            Thanks to torch, slightly modified
+    Args:
+        url (string): URL of the object to download
+        dst (string): Full path where object will be saved, e.g. `/tmp/temporary_file`
+        progress (bool, optional): whether or not to display a progress bar to stderr
+            Default: True
+    """
+    file_size = None
+    u = urlopen(url)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+    # We deliberately save it in a temp file and move it after
+    dst = os.path.expanduser(dst)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+    try:
+        with tqdm(total=file_size, disable=not progress,
+                  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                pbar.update(len(buffer))
+        f.close()
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)

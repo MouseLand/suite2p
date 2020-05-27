ef797e38aa88937e1f169c8cdbb751710e04852e
Chris Ki <chriski777@berkeley.edu>
2020-05-27 13:38:42 -0700

    Changed test_data structure. Refactored test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -12,35 +12,6 @@ test_data_dir = suite_dir
 print(test_data_dir)
 
 
-def compare_npy_arrays(arr1, arr2, rtol, atol):
-    """
-    Compares contents of two numpy arrays. Checks that the elements are within an absolute tolerance of 0.008.
-
-    Parameters
-    ----------
-    arr1
-    arr2
-    atol
-
-    Returns
-    -------
-
-    """
-    # Handle cases where the elements of npy arrays are dictionaries (e.g: stat.npy)
-    if arr1.dtype == np.dtype('object') and (arr2.dtype == np.dtype('object')):
-        # Iterate through dictionaries
-        for i in range(len(arr1)):
-            gt_curr_dict = arr1[i]
-            output_curr_dict = arr2[i]
-            # Iterate through keys and make sure values are same
-            for k in gt_curr_dict.keys():
-                assert np.allclose(gt_curr_dict[k], output_curr_dict[k], rtol=rtol, atol=atol)
-    # Assume all other cases have arrays that contain numbers
-    else:
-        print("Max Absolute diff is : {}".format(np.max(np.abs(arr1-arr2))))
-        print("Min rtol*(abs(arr2)) is : {}".format(np.min(rtol*np.abs(arr2))))
-        assert np.allclose(arr1, arr2, rtol=rtol, atol=atol)
-
 
 @pytest.fixture()
 def setup_and_teardown(tmpdir):
@@ -50,13 +21,14 @@ def setup_and_teardown(tmpdir):
     """
     ops = suite2p.default_ops()
     ops['data_path'] = [test_data_dir]
-    ops['save_path0'] = str(tmpdir)
+    ops['save_path0'] = '/Users/chriski/Desktop/suite2p_test_output'
     yield ops, str(tmpdir)
-    tmpdir_path = Path(str(tmpdir))
-    if tmpdir_path.is_dir():
-        shutil.rmtree(tmpdir)
-        print('Successful removal of tmp_path {}.'.format(tmpdir))
-    
+    # tmpdir_path = Path(str(tmpdir))
+    # if tmpdir_path.is_dir():
+    #     shutil.rmtree(tmpdir)
+    #     print('Successful removal of tmp_path {}.'.format(tmpdir))
+
+
 class TestCommonPipelineUseCases:
     """
     Class that tests common use cases for pipeline.
@@ -68,12 +40,9 @@ class TestCommonPipelineUseCases:
         as the ground truth outputs.
         """
         output_dir = Path(output_root).joinpath("suite2p")
-        # Output has different structure from test_data structure
         outputs_to_check = ['F', 'Fneu', 'iscell', 'spks', 'stat']
-        # Check channel2 outputs if necessary
         if nchannels == 2:
             outputs_to_check.extend(['F_chan2', 'Fneu_chan2'])
-        # Go through each plane folder and compare contents
         for i in range(nplanes):
             for output in outputs_to_check:
                 test_data = np.load(
@@ -85,7 +54,14 @@ class TestCommonPipelineUseCases:
                     str(output_dir.joinpath('plane{}'.format(i), "{}.npy".format(output))), allow_pickle=True
                 )
                 print("Comparing {} for plane {}".format(output, i))
-                compare_npy_arrays(test_data, output_data, rtol=1e-6, atol=5e-2)
+                rtol, atol = 1e-6 , 5e-2
+                # Handle cases where the elements of npy arrays are dictionaries (e.g: stat.npy)
+                if output == 'stat':
+                    for gt_dict, output_dict in zip(test_data, output):
+                        for k in gt_dict.keys():
+                            assert np.allclose(gt_dict[k], output_dict[k], rtol=rtol, atol=atol)
+                else:
+                    assert np.allclose(test_data, output_data)
 
     def test_1plane_1chan(self, setup_and_teardown):
         """
@@ -93,6 +69,7 @@ class TestCommonPipelineUseCases:
         """
         # Get ops and unique tmp_dir from fixture
         ops, tmp_dir = setup_and_teardown
+        ops['save_path0'] = ops['save_path0'] + '/1plane1chan'
         suite2p.run_s2p(ops=ops)
         # Check outputs against ground_truth files
         self.check_output_gt(tmp_dir, ops['nplanes'], ops['nchannels'])
@@ -103,6 +80,7 @@ class TestCommonPipelineUseCases:
         """
         ops, tmp_dir = setup_and_teardown
         ops['nplanes'] = 2
+        ops['save_path0'] = ops['save_path0'] + '/2plane1chan'
         suite2p.run_s2p(ops=ops)
         self.check_output_gt(tmp_dir, ops['nplanes'], ops['nchannels'])
 
@@ -113,5 +91,6 @@ class TestCommonPipelineUseCases:
         ops, tmp_dir = setup_and_teardown
         ops['nplanes'] = 2
         ops['nchannels'] = 2
+        ops['save_path0'] = ops['save_path0'] + '/2plane2chan'
         suite2p.run_s2p(ops=ops)
         self.check_output_gt(tmp_dir, ops['nplanes'], ops['nchannels'])

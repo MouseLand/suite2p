5f5c82f07f4b17bc2d2b3001966fed334884752d
Chris Ki <chriski777@berkeley.edu>
2020-05-29 08:07:51 -0700

    Initialized module tests.

---
+++ tests/test_modules/test_io_module.py
@@ -0,0 +1,11 @@
+import numpy as np
+import suite2p
+
+
+class TestSuite2pIoModule:
+    """
+    Tests for the Suite2p IO module
+    """
+    def test_io_module_input_output(self, setup_and_teardown, get_test_dir_path):
+        pass
+
---
+++ tests/test_modules/test_registration_module.py
@@ -0,0 +1,10 @@
+import numpy as np
+import suite2p
+
+
+class TestSuite2pRegistrationModule:
+    """
+    Tests for the Suite2p Registration Module
+    """
+    def test_registration_module_input_output(self, setup_and_teardown, get_test_dir_path):
+        pass

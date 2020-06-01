9f601bacb14e94513f4537147b1f9552a77bc73d
Chris Ki <chriski777@berkeley.edu>
2020-06-01 00:24:36 -0700

    Added additional checks for register_binary_rigid_registration.

--- tests/test_modules/test_registration_module.py
+++ tests/test_modules/test_registration_module.py
@@ -34,12 +34,27 @@ class TestSuite2pRegistrationModule:
         Tests that register_binary works for a dataset that only has rigid shifts.
         """
         op, tmp_dir = setup_and_teardown
+        op['nonrigid'] = False
         op = prepare_for_registration(op)
         op = registration.register_binary(op)
         registered_data = imread(str(Path(op['save_path']).joinpath('reg_tif', 'file000_chan0.tif')))
         # Make sure registered_data is identical across frames
         check_data = np.repeat(registered_data[0, :, :][np.newaxis, :, :], 500, axis=0)
         assert np.array_equal(check_data, registered_data)
+        # Check and see if there are exactly 16 lines row-wise and column-wise
+        num_row_lines = len(np.where(np.all(np.all(check_data == 1500, axis=0), axis=0))[0])
+        num_col_lines = len(np.where(np.all(np.all(check_data == 1500, axis=0), axis=1))[0])
+        assert num_col_lines == 16
+        assert num_row_lines == 16
 
-    def test_register_binary_nonrigid_registration_only(selfself, setup_and_teardown):
+    def test_register_binary_nonrigid_registration_only(self, setup_and_teardown):
+        """
+        Tests that register_binary works for a dataset that only has non-rigid shifts.
+        """
+        pass
+
+    def test_register_binary_nonrigid_rigid_registration(self, setup_and_teardown):
+        """
+        Tests that register_binary works for a dataset that has both non-rigid and rigid shifts.
+        """
         pass

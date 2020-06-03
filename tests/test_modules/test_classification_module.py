3f16ebee0e7c7768032b877d5e5e7a79792a6fef
Chris Ki <chriski777@berkeley.edu>
2020-06-02 19:04:06 -0700

    Added regression test for classification module. Added tailored stat.npy that's used before classifier is called.

--- tests/test_modules/test_classification_module.py
+++ tests/test_modules/test_classification_module.py
@@ -1,10 +1,19 @@
 import numpy as np
+from pathlib import Path
 from suite2p import classification
 
 
 class TestSuite2pClassificationModule:
     """
-    Tests for the Suite2p Classification module
+    Tests for the Suite2p Classification module.
     """
-    def test_classification_module_input_output(self, setup_and_teardown, get_test_dir_path):
-        pass
+    def test_classification_output(self, setup_and_teardown, get_test_dir_path):
+        """
+        Regression test that checks the output of classification.
+        """
+        op, tmp_dir = setup_and_teardown
+        default_cls_file = get_test_dir_path.parent.parent.joinpath('suite2p', 'classifiers', 'classifier.npy')
+        stat = np.load(get_test_dir_path.joinpath('classification', 'pre_stat.npy'), allow_pickle=True)
+        iscell = classification.Classifier(default_cls_file, keys=['npix_norm', 'compact', 'skew']).run(stat)
+        expected_output = np.load(get_test_dir_path.joinpath('1plane1chan', 'suite2p', 'plane0', 'iscell.npy'))
+        assert np.array_equal(iscell, expected_output)

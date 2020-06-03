ef0b5de8657e70378ba0444016dc5a0c3ed8bca5
Harrison <chriski777@berkeley.edu>
2020-06-02 20:30:20 -0700

    Modified tolerance value after checking on another computer.

--- tests/test_modules/test_classification_module.py
+++ tests/test_modules/test_classification_module.py
@@ -17,4 +17,4 @@ class TestSuite2pClassificationModule:
         iscell = classification.Classifier(default_cls_file, keys=['npix_norm', 'compact', 'skew']).run(stat)
         expected_output = np.load(get_test_dir_path.joinpath('1plane1chan', 'suite2p', 'plane0', 'iscell.npy'))
         # Logistic Regression has differences in tolerance due to depndence on C
-        assert np.allclose(iscell, expected_output, atol=1e-4)
+        assert np.allclose(iscell, expected_output, atol=2e-4)

f6ff7cead2fda60fc1d23021aadaf7a334f953a1
Chris Ki <chriski777@berkeley.edu>
2020-06-23 16:15:55 -0700

    Implement click package in registration_metrics script

--- benchmarks/registration_metrics.py
+++ benchmarks/registration_metrics.py
@@ -1,45 +1,34 @@
-import sys, getopt
+import click
 from pathlib import Path
 
 
-def invalid_argument():
-    print("ERROR: Please provide a valid input argument.")
-    print('registration_metrics.py -i <input_tif_file_path>')
-    sys.exit(2)
+@click.command()
+@click.argument('inputfile', type=click.Path(exists=True))
+@click.argument('outputpath', type=click.Path(exists=True))
+def run_pc_reg_metrics(inputfile, outputpath):
+    """
+    Returns registration PC statistics for given INPUTFILE. Stores output of suite2p pipeline in
+    OUTPUTPATH.
 
-
-def main(argv):
-    input_path = ''
-    input_file = None
-    try:
-        opts, args = getopt.getopt(argv, "hi:")
-    except getopt.GetoptError:
-        invalid_argument()
-    # Go through command line arguments
-    for opt, arg in opts:
-        if opt == '-h':
-            print('usage: registration_metrics.py -i <input_tif_file_path>')
-            sys.exit()
-        elif opt in '-i':
-            input_path = arg
-            input_file = Path(input_path)
-    # Check if supplied input_file is
-    if input_file and input_file.exists():
-        print("Input file is : {}".format(input_file))
-        print("Importing suite2p...")
-        # Only import if input properly specified
-        import suite2p
-        default_ops = suite2p.default_ops()
-        default_ops['do_regmetrics'] = True
-        default_ops['roidetect'] = False
-        default_ops['data_path'] = [input_file.parent]
-        default_ops['tiff_list'] = [str(input_file.name)]
-        default_ops['save_path0'] = '/Users/chriski/Desktop/suite2p_ws'
-        print("Calculating registration metrics... ")
-        suite2p.run_s2p(default_ops)
-    else:
-        invalid_argument()
+    \b
+    Arguments:
+        INPUTFILE is the tif you want to calculate registration metrics for.
+        OUTPUTHPATH is the path where the output of suite2p is stored.
+    """
+    click.echo("Input file is: {} ".format(click.format_filename(inputfile)))
+    click.echo("Output path is: {}".format(click.format_filename(outputpath)))
+    click.echo("Importing suite2p...")
+    import suite2p
+    # Assign default ops for suite2p
+    default_ops = suite2p.default_ops()
+    default_ops['do_regmetrics'] = True
+    default_ops['roidetect'] = False
+    default_ops['data_path'] = [Path(inputfile).parent]
+    default_ops['tiff_list'] = [inputfile]
+    default_ops['save_path0'] = outputpath
+    click.echo("Calculating registration metrics...")
+    result_ops = suite2p.run_s2p(default_ops)
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    run_pc_reg_metrics()

fixed normalization order in sd_mov()

suite2p/detection/sourcery.py
@@ -37,7 +37,7 @@ def getSVDdata(ops):
         mov[j,:,:] = gaussian_filter(mov[j,:,:], sig)
 
     # compute noise variance across frames
-    sdmov = utils.get_sdmov(mov, ops)
+    sdmov = utils.standard_deviation_over_time(mov, batch_size=ops['batch_size'])
     mov /= sdmov
     mov = np.reshape(mov, (-1,Lyc*Lxc))
     # compute covariance of binned frames
@@ -80,7 +80,7 @@ def getSVDproj(ops, u):
             mov[j,:,:] = gaussian_filter(mov[j,:,:], sig)
     if 1:
         #sdmov = np.ones((Lyc*Lxc,), 'float32')
-        sdmov = utils.get_sdmov(mov, ops)
+        sdmov = utils.standard_deviation_over_time(mov, batch_size=ops['batch_size'])
         mov/=sdmov
         mov = np.reshape(mov, (-1,Lyc*Lxc))
 

suite2p/detection/sparsedetect.py
@@ -1,6 +1,8 @@
 import time
 
 import numpy as np
+from numpy.linalg import norm
+
 from scipy.interpolate import RectBivariateSpline
 from scipy.ndimage import maximum_filter
 from scipy.ndimage.filters import uniform_filter
@@ -83,7 +85,7 @@ def add_square(yi,xi,lx,Ly,Lx):
 
     """
     lhf = int((lx-1)/2)
-    ipix = np.arange(-lhf,-lhf+lx)+ np.zeros(lx, 'int32')[:, np.newaxis]
+    ipix = np.tile(np.arange(-lhf, -lhf + lx, dtype=np.int32), reps=(lx, 1))
     x0 = xi + ipix
     y0 = yi + ipix.T
     mask  = np.ones((lx,lx), 'float32')
@@ -91,7 +93,7 @@ def add_square(yi,xi,lx,Ly,Lx):
     x0 = x0[ix]
     y0 = y0[ix]
     mask = mask[ix]
-    mask = mask / (mask**2).sum()**.5
+    mask = mask / norm(mask)
     return y0.flatten(), x0.flatten(), mask.flatten()
 
 def iter_extend(ypix, xpix, rez, Lyc,Lxc):
@@ -292,7 +294,7 @@ def sparsery(ops):
     # cropped size
     ops['Lyc'] = Lyc
     ops['Lxc'] = Lxc
-    sdmov = utils.get_sdmov(rez, ops)
+    sdmov = utils.standard_deviation_over_time(rez, batch_size=ops['batch_size'])
     rez /= sdmov
     
     # subtract low-pass filtered version of binned movie

suite2p/detection/utils.py
@@ -68,37 +68,14 @@ def high_pass_rolling_mean_filter(mov: np.ndarray, width: int) -> np.ndarray:
     return mov
 
 
-def get_sdmov(mov, ops):
-    """ computes standard deviation of difference between pixels across time
-
-    difference between frames in binned movie computed then stddev
-    helps to normalize image across pixels
-
-    Parameters
-    ----------------
-
-    mov : 3D array
-        size [nbins x Ly x Lx]
-
-    ops : dictionary
-        'batch_size'
-
-    stat : array of dicts
-        'ypix', 'xpix'
-
-    Returns
-    ----------------
-
-    stat : array of dicts
-        adds 'overlap'
-
-    """
+def standard_deviation_over_time(mov: np.ndarray, batch_size: int) -> np.ndarray:
+    """Returns standard deviation of difference between pixels across time, computed in batches of batch_size."""
     nbins, Ly, Lx = mov.shape
-    batch_size = min(ops['batch_size'], nbins)
+    batch_size = min(batch_size, nbins)
     sdmov = np.zeros((Ly, Lx), 'float32')
     for ix in range(0, nbins, batch_size):
-        sdmov += ((np.diff(mov[ix:ix+batch_size, :, :], axis=0) ** 2).sum(axis=0) / nbins) ** 0.5
-    sdmov = np.maximum(1e-10, sdmov)
+        sdmov += ((np.diff(mov[ix:ix+batch_size, :, :], axis=0) ** 2).sum(axis=0))
+    sdmov = np.maximum(1e-10, np.sqrt(sdmov / nbins))
     return sdmov
 
 
8085383a7e4456430c902c174d89f2734a3ef808
Chris Ki <chriski777@berkeley.edu>
2020-06-27 19:42:36 -0700

    Added nwb test to 1plane1chan test on pipeline tests.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -2,6 +2,9 @@
 Class that tests common use cases for pipeline.
 """
 
+from suite2p import io
+from pathlib import Path
+import numpy as np
 import suite2p
 import utils
 
@@ -13,20 +16,34 @@ def get_outputs_to_check(n_channels):
     return outputs_to_check
 
 
-def test_1plane_1chan_with_batches_and_metrics(default_ops):
+def test_1plane_1chan_with_batches_metrics_and_exported_to_nwb_format(default_ops):
     """
-    Tests for case with 1 plane and 1 channel with multiple batches.
+    Tests for case with 1 plane and 1 channel with multiple batches. Results are saved to nwb format
+    then checked to see if it contains the necessary parts for use with GUI.
     """
     default_ops['tiff_list'] = ['input_1500.tif']
     default_ops['do_regmetrics'] = True
+    default_ops['save_NWB'] = True
+    outputs_to_check = ['F', 'Fneu', 'spks', 'iscell']
     suite2p.run_s2p(ops=default_ops)
     utils.check_output(
         default_ops['save_path0'],
-        get_outputs_to_check(default_ops['nchannels']),
+        outputs_to_check,
         default_ops['data_path'][0],
         default_ops['nplanes'],
         default_ops['nchannels'],
         added_tag='1500'
+    )
+    # Read Nwb data and make sure it's identical to output data
+    stat, ops, F, Fneu, spks, iscell, probcell, redcell, probredcell = \
+        io.read_nwb(str(Path(default_ops['save_path0']).joinpath('suite2p').joinpath('ophys.nwb')))
+    utils.compare_list_of_outputs(
+        0,
+        outputs_to_check,
+        utils.get_list_of_output_data(outputs_to_check, default_ops['save_path0'], 0),
+        [F, Fneu, spks,
+         np.concatenate([iscell.astype(np.float32)[:,np.newaxis], probcell.astype(np.float32)[:,np.newaxis]], axis=1)
+        ]
     )
 
 

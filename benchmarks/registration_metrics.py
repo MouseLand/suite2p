e9a5e11aa78d1c11ae97a70a0adc000cd210c270
Chris Ki <chriski777@berkeley.edu>
2020-07-02 01:49:28 -0700

    Removed dependence of benchmark script on click package. Instead relying on main cli script in suite2p.

--- benchmarks/registration_metrics.py
+++ benchmarks/registration_metrics.py
@@ -1,63 +1,37 @@
-import click
-from pathlib import Path
+import argparse
+import suite2p
+from suite2p.__main__ import add_args, parse_args
 
 
-@click.command()
-@click.argument('inputfile', type=click.Path(exists=True))
-@click.argument('outputpath', type=click.Path(exists=True))
-@click.option('-nc', '--nchannels', default=1, show_default=True, help="Number of channels")
-@click.option('-np', '--nplanes', default=1, show_default=True, help="Number of planes")
-@click.option('-op', '--one_preg', default=False, show_default=True,
-              help="Whether to perform high-pass filtering and tapering")
-@click.option('-bdc', '--bidi_corrected', default=False, show_default=True, help="Bidirectional phase offset correction")
-@click.option('-ss', '--smooth_sigma', default=1.15, show_default=True, help="Sigma for smoothing in time")
-@click.option('-shp', '--spatial_hp_reg', default=26, show_default=True, help="Window for spatial high-pass filtering")
-@click.option('-ps', '--pre_smooth', default=1, show_default=True,
-              help="Whether to smooth before high-pass filtering before registration")
-@click.option('-st', '--spatial_taper', default=50, show_default=True,
-              help="How much to ignore on edges (important for vignetted windows,for FFT padding do not set BELOW 3*ops['smooth_sigma']")
-def run_pc_reg_metrics(inputfile, outputpath, nchannels, nplanes, one_preg,
-                       bidi_corrected, smooth_sigma, spatial_hp_reg, pre_smooth, spatial_taper):
-    """
-    Returns registration PC statistics for given INPUTFILE. Stores output of suite2p pipeline in
-    OUTPUTPATH.
+def set_op_param(ops, args, param_name_list):
+    for p_name in param_name_list:
+        pval = getattr(args, p_name)
+        if pval:
+            ops[p_name] = pval
+            print('->> Setting {0} to {1}'.format(p_name, ops[p_name]))
+    return ops
 
-    \b
-    Arguments:
-        INPUTFILE is the tif you want to calculate registration metrics for.
-        OUTPUTHPATH is the path where the output of suite2p is stored.
-    """
-    click.echo("Input file is: {} ".format(click.format_filename(inputfile)))
-    click.echo("Output path is: {}".format(click.format_filename(outputpath)))
-    click.echo("Importing suite2p...")
-    import suite2p
-    # Assign necessary parameters to default ops for suite2p
-    default_ops = suite2p.default_ops()
-    default_ops['do_regmetrics'] = True
-    default_ops['roidetect'] = False
-    default_ops['data_path'] = [Path(inputfile).parent]
-    default_ops['tiff_list'] = [inputfile]
-    default_ops['save_path0'] = outputpath
-    # Assign optional ops for suite2p
-    default_ops['nplanes'] = nplanes
-    default_ops['nchannels'] = nchannels
-    default_ops['1Preg'] = one_preg
-    default_ops['bidi_corrected'] = bidi_corrected
-    default_ops['smooth_sigma'] = smooth_sigma
-    default_ops['spatial_hp_reg'] = spatial_hp_reg
-    default_ops['pre_smooth'] = pre_smooth
-    default_ops['spatial_taper'] = spatial_taper
-    click.echo("Calculating registration metrics...")
-    result_ops = suite2p.run_s2p(default_ops)
-    for plane in range(nplanes):
-        dx = result_ops[plane]['regDX']
-        reg_pc =result_ops[plane]['regPC']
-        tpc = result_ops[plane]['tPC']
+
+def registration_metrics():
+    default_parser = add_args(argparse.ArgumentParser(description='Suite2p parameters'))
+    default_parser.add_argument('data_path', type=str, nargs=1, help='Path to directory with input files')
+    default_parser.add_argument('--tiff_list', default=[], type=str, nargs='*', help='Input files selected')
+    args, ops = parse_args(default_parser)
+    # Set registration metrics specific parameters
+    ops['do_regmetrics'] = True
+    ops['roidetect'] = False
+    ops = set_op_param(ops, args, ['data_path', 'tiff_list'])
+    print("Calculating registration metrics...")
+    result_ops = suite2p.run_s2p(ops)
+    for p in range(ops['nplanes']):
+        dx = result_ops[p]['regDX']
+        reg_pc = result_ops[p]['regPC']
+        tpc = result_ops[p]['tPC']
         bottom_pc = reg_pc[0]
         top_pc = reg_pc[1]
-        click.echo(bottom_pc.shape)
-        click.echo(top_pc.shape)
+        print(top_pc.shape)
+        print(bottom_pc.shape)
 
 
 if __name__ == "__main__":
-    run_pc_reg_metrics()
+    registration_metrics()

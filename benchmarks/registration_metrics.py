5a474a8483bcfc3945110be0d56f18a1db37fcb4
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-07-14 20:52:54 +0200

    Refactored registration_metrics into View-Model-Presenter functions and added reg_metrics entry point

--- benchmarks/registration_metrics.py
+++ benchmarks/registration_metrics.py
@@ -1,45 +1,52 @@
 import argparse
 import suite2p
 import numpy as np
+from typing import NamedTuple
 
 from suite2p.__main__ import add_args, parse_args
 
 
-def print_iter(iter_list, iter_name_list, pre=""):
-    for val, name in zip(iter_list, iter_name_list):
-        print("{} offsets: {}".format(pre + " " + name, val))
+class RegMetricResult(NamedTuple):
+    nplane: int
+    avg_offs: np.array
+    max_offs: np.array
 
 
-def registration_metrics():
+def registration_metrics(data_path, tiff_list, ops, nPC = 10):
     """
     Displays registration offsets calculated on pclow and pchigh frames. If registration was performed well,
     the PCs should not contain movement. All offsets calculated on pclow/pchigh frames should be close to zero.
     """
+    ops['do_regmetrics'] = True
+    ops['roidetect'] = False
+    ops['reg_metric_n_pc'] = nPC
+    ops['data_path'] = data_path
+    ops['tiff_list'] = tiff_list
+
+    result_ops = suite2p.run_s2p(ops)
+    metric_results = []
+    for nplane, result_op in enumerate(result_ops):
+        offsets = result_op['regDX']
+        result = RegMetricResult(nplane=nplane, avg_offs=np.mean(offsets, axis=0), max_offs=np.max(offsets, axis=0))
+        metric_results.append(result)
+    return metric_results
+
+
+def main():
     default_parser = add_args(argparse.ArgumentParser(description='Suite2p parameters'))
     default_parser.add_argument('data_path', type=str, nargs=1, help='Path to directory with input files')
     default_parser.add_argument('--tiff_list', default=[], type=str, nargs='*', help='Input files selected')
     args, ops = parse_args(default_parser)
-    # Set registration metrics specific parameters
-    ops['do_regmetrics'] = True
-    ops['roidetect'] = False
-    ops['reg_metric_n_pc'] = 10
-    # Sets each parameter name to corresponding args value for ops dictionary
-    for p_name in ['data_path', 'tiff_list']:
-        pval = getattr(args, p_name)
-        if pval:
-            ops[p_name] = pval
-            print('->> Setting {0} to {1}'.format(p_name, ops[p_name]))
-    print("Calculating registration metrics...")
-    result_ops = suite2p.run_s2p(ops)
-    off_strs = ['rigid', 'avg_non_rigid', 'max_non_rigid']
-    for p in range(ops['nplanes']):
-        print("\nPlane {}: ".format(p))
-        offsets = result_ops[p]['regDX']
-        avg_offs = np.mean(offsets, axis=0)
-        max_offs = np.max(offsets, axis=0)
-        print_iter(avg_offs, off_strs, 'Average')
-        print_iter(max_offs, off_strs, 'Max')
+    reg_metric_results = registration_metrics(data_path=args.data_path, tiff_list=args.tiff_list, ops=ops)
+    for r in reg_metric_results:
+        print(f"""
+        Plane {r.nplane}:
+        Averages:
+        Rigid: {r.avg_offs[0]} \tAverage NR: {r.avg_offs[1]} \tMax NR: {r.avg_offs[2]}
+        Max:
+        Rigid: {r.max_offs[0]} \tAverage NR: {r.max_offs[1]} \tMax NR: {r.max_offs[2]}
+        """)
 
 
 if __name__ == "__main__":
-    registration_metrics()
+    main()

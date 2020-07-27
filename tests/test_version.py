00658b28d00ccce921b60635107e0f1f28ffbcf4
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-07-27 14:52:29 +0200

    added --version option to CLI

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,14 @@
-import suite2p
+import os
 from importlib_metadata import metadata
+
+import suite2p
 
 
 def test_package_version_number_matches_setuptool_version_number():
     assert suite2p.version == metadata('suite2p')['version']
 
 
+def test_cli_provides_correct_package_number(capfd):
+    os.system('suite2p --version')
+    captured = capfd.readouterr()
+    assert metadata('suite2p')['version'] in captured.out

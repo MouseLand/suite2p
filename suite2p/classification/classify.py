4ddc446f3f6258d1e621aa889e1d772e53de63af
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-07-31 18:43:42 +0200

    Flattened nested conditionals, removed function get_builtin_classfile(), made consistent structure within conditional block.
    Co-authored-by: Chris Ki <chriski777@berkeley.edu>
    Co-authored-by: Nicholas A. Del Grosso <delgrosso.nick@gmail.com>

--- suite2p/classification/classify.py
+++ suite2p/classification/classify.py
@@ -4,25 +4,23 @@ from typing import Union, Sequence
 from .classifier import Classifier
 
 
-def get_built_in_classifier_path() -> Path:
-    print('NOTE: applying built-in classifier.npy')
-    return Path(__file__).joinpath('../../classifiers/classifier.npy').resolve()
-
-
 def classify(stat: np.ndarray, use_builtin_classifier: bool = False,
              classfile: Union[str, Path] = None, keys: Sequence[str] = ('npix_norm', 'compact', 'skew'),
              ):
     """Returns array of classifier output from classification process."""
-
+    builtin_classfile = Path(__file__).joinpath('../../classifiers/classifier.npy').resolve()
+    user_classfile = Path.home().joinpath('.suite2p', 'classifiers', 'classifier_user.npy')
     if use_builtin_classifier:
-        classfile = get_built_in_classifier_path()
+        print(f'NOTE: Applying builtin classifier at {str(builtin_classfile)}')
+        classfile = builtin_classfile
+    elif not user_classfile.is_file():
+        print(f'NOTE: no user default classifier exists.  applying builtin classifier at {str(builtin_classfile)}')
+        classfile = builtin_classfile
     elif classfile is None or not Path(classfile).is_file():
-        print('NOTE: applying default $HOME/.suite2p/classifiers/classifier_user.npy')
-        classfile = Path.home().joinpath('.suite2p', 'classifiers', 'classifier_user.npy')
-        if not Path(classfile).is_file():
-            print('(no user default classifier exists)')
-            classfile = get_built_in_classifier_path()
+        print(f'NOTE: applying default {str(user_classfile)}')
+        classfile = user_classfile
     else:
-        print('NOTE: applying classifier %s' % classfile)
+        print(f'NOTE: applying classifier {str(classfile)}')
+        classfile = classfile
     keys = list(set(keys).intersection(set(stat[0])))
     return Classifier(classfile, keys=keys).run(stat)

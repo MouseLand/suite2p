6d87c6742c90b29354a939f4a9664ad076101bee
Chris Ki <chriski777@berkeley.edu>
2020-08-04 19:59:06 -0700

    Added smoke test for suite2p command in CLI

--- tests/smoke/test_cli.py
+++ tests/smoke/test_cli.py
@@ -1,5 +1,6 @@
 import os
-
+import subprocess
+from time import sleep
 
 def test_cli_help_test_appears_when_suite2p_is_called(capfd):
     os.system('suite2p --help')
@@ -41,3 +42,11 @@ def test_cli_reg_metrics_runs_with_output(capfd, tmpdir):
     os.system(f'reg_metrics data/test_data/ --tiff_list input_1500.tif --do_registration 2 --save_path0 {tmpdir}')
     captured = capfd.readouterr()
     assert captured.out
+
+
+def test_cli_suite2p_gui_runs_when_is_called_locally():
+    s2p = subprocess.Popen(['python', '-m', 'suite2p'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    sleep(10)
+    s2p.terminate()
+    output, err = s2p.communicate(b'\n')
+    assert "Error" not in err.decode('ascii')

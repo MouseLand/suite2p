7d4e6dc49c2e59773c63f27e0aada5bdacd4d178
Chris Ki <chriski777@berkeley.edu>
2020-08-08 22:47:23 -0700

    Added poll command to cli smoke test in test_cli.py

--- tests/smoke/test_cli.py
+++ tests/smoke/test_cli.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 from time import sleep
 
+
 def test_cli_help_test_appears_when_suite2p_is_called(capfd):
     os.system('suite2p --help')
     captured = capfd.readouterr()
@@ -47,6 +48,7 @@ def test_cli_reg_metrics_runs_with_output(capfd, tmpdir):
 def test_cli_suite2p_gui_runs_when_is_called_locally():
     s2p = subprocess.Popen(['python', '-m', 'suite2p'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     sleep(10)
+    assert s2p.poll() is None  # Make sure process is running
     s2p.terminate()
     output, err = s2p.communicate(b'\n')
     assert "Error" not in err.decode('ascii')

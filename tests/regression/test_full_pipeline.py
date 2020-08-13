f39e15e8c1cada0d58dc816f00c2f9af3a1b718b
Chris Ki <chriski777@berkeley.edu>
2020-08-12 20:25:22 -0700

    check all outputs for 1plane1channwb test

--- tests/regression/test_full_pipeline.py
+++ tests/regression/test_full_pipeline.py
@@ -27,23 +27,21 @@ def test_1plane_1chan_with_batches_metrics_and_exported_to_nwb_format(test_ops):
     })
     suite2p.run_s2p(ops=test_ops)
 
-    outputs_to_check = ['F', 'Fneu', 'spks', 'iscell']
     nplanes = test_ops['nplanes']
     assert all(utils.check_output(
         output_root=test_ops['save_path0'],
-        outputs_to_check=outputs_to_check,
+        outputs_to_check=get_outputs_to_check(test_ops['nchannels']),
         test_data_dir=test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan1500/suite2p/"),
         nplanes=nplanes,
     ))
-
     # Read Nwb data and make sure it's identical to output data
     stat, ops, F, Fneu, spks, iscell, probcell, redcell, probredcell = \
         io.read_nwb(str(Path(test_ops['save_path0']).joinpath('suite2p/ophys.nwb')))
     assert all(utils.compare_list_of_outputs(
         0,
-        outputs_to_check,
-        utils.get_list_of_output_data(outputs_to_check, test_ops['save_path0'], 0),
-        [F, Fneu, spks, np.stack([iscell.astype(np.float32), probcell.astype(np.float32)]).T],
+        get_outputs_to_check(test_ops['nchannels']),
+        utils.get_list_of_output_data(get_outputs_to_check(test_ops['nchannels']), test_ops['save_path0'], 0),
+        [F, Fneu, np.stack([iscell.astype(np.float32), probcell.astype(np.float32)]).T, spks, stat],
     ))
 
 

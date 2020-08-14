670760164b3176a9b0bd29dcd73b7a31a66fbd22
Chris Ki <chriski777@berkeley.edu>
2020-08-13 18:22:02 -0700

    Add params to check_output_calls, also fix nplanes for 2plane2chan test.

--- tests/regression/test_full_pipeline.py
+++ tests/regression/test_full_pipeline.py
@@ -31,10 +31,10 @@ def test_1plane_1chan_with_batches_metrics_and_exported_to_nwb_format(test_ops):
     outputs_to_check = ['F', 'Fneu', 'spks', 'iscell']
     nplanes = test_ops['nplanes']
     assert all(utils.check_output(
-        test_ops['save_path0'],
-        outputs_to_check,
-        test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan1500/suite2p/"),
-        nplanes,
+        output_root=test_ops['save_path0'],
+        outputs_to_check=outputs_to_check,
+        test_data_dir=test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan1500/suite2p/"),
+        nplanes=nplanes,
     ))
 
     # Read Nwb data and make sure it's identical to output data
@@ -62,16 +62,14 @@ def test_2plane_2chan_with_batches(test_ops):
             'reg_tif': True,
             'reg_tif_chan2': True,
         })
-        nplanes = test_ops['nplanes']
+        nplanes = ops['nplanes']
         suite2p.run_s2p(ops=ops)
         assert all(utils.check_output(
-            ops['save_path0'],
-            get_outputs_to_check(ops['nchannels']) + ['reg_tif', 'reg_tif_chan2'],
-            test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan1500/suite2p/"),
-            nplanes,
+            output_root=test_ops['save_path0'],
+            outputs_to_check=get_outputs_to_check(test_ops['nchannels']) + ['reg_tif', 'reg_tif_chan2'],
+            test_data_dir=test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan1500/suite2p/"),
+            nplanes=nplanes,
         ))
-
-
 
 
 def test_1plane_2chan_sourcery(test_ops):
@@ -87,8 +85,8 @@ def test_1plane_2chan_sourcery(test_ops):
     suite2p.run_s2p(ops=test_ops)
     nplanes = test_ops['nplanes']
     assert all(utils.check_output(
-        test_ops['save_path0'],
-        get_outputs_to_check(test_ops['nchannels']),
-        test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan/suite2p/"),
-        nplanes,
+        output_root=test_ops['save_path0'],
+        outputs_to_check=get_outputs_to_check(test_ops['nchannels']),
+        test_data_dir=test_ops['data_path'][0].joinpath(f"{nplanes}plane{test_ops['nchannels']}chan/suite2p/"),
+        nplanes=nplanes,
     ))

3f704381cda58a86d9d1101398af7d191962e361
Carsen Stringer <carsen.stringer@gmail.com>
2021-07-20 12:14:33 -0400

    removing gui test for now

--- tests/smoke/test_gui.py
+++
@@ -1,7 +0,0 @@
-from suite2p.gui.gui2p import MainWindow
-
-pytest_plugins = 'pytestqt.plugin'
-
-def test_main_window_launches_without_error(qtbot):
-    app = MainWindow()
-    qtbot.addWidget(app)

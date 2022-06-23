6f14d2d8dbfcba6a3fb7b5686ba9f7b22c324a06
Chris Ki <cski@andrew.cmu.edu>
2022-06-22 21:30:47 -0400

    Finished classification test data generation

--- tests/regression/test_classification_pipeline.py
+++ tests/regression/test_classification_pipeline.py
@@ -9,7 +9,7 @@ from suite2p import classification
 def get_stat_iscell(data_dir_path):
     # stat with standard deviation and skew already calculated
     stat = np.load(data_dir_path.joinpath('classification/pre_stat.npy'), allow_pickle=True)
-    expected_output = np.load(data_dir_path.joinpath('1plane1chan/suite2p/plane0/iscell.npy'))
+    expected_output = np.load(data_dir_path.joinpath('classification/expected_classify_output_1p1c0.npy'))
     return stat, expected_output
 
 

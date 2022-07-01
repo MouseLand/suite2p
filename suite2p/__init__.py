cf6a81263104b528154979157273c5a2295aa3ce
Chris Ki <cski@andrew.cmu.edu>
2022-07-01 17:32:29 -0400

    Added wrapper functions to suite2p init file for easier asccess

--- suite2p/__init__.py
+++ suite2p/__init__.py
@@ -1,6 +1,9 @@
 from .version import version
 from .default_ops import default_ops
 from .run_s2p import run_s2p
-from .detection import ROI
+from .detection import ROI, detection_wrapper
+from .classification import classify
+from .extraction import extraction_wrapper
+from .registration import registration_wrapper
 
 name = "suite2p"

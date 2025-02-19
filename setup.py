a0de6c2932608f367b2d066a324ed5ef97c4aed9
AhmedJ <ahmedj@stud.ntnu.no>
2025-02-19 14:28:31 +0100

    Changes to setup.py and README

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
 import setuptools
 
-install_deps = ["importlib-metadata",
-        "natsort",
-        "rastermap>=0.9.0",
-        "tifffile",
-        "torch>=1.13.1",
-        "numpy>=1.24.3",
-        "numba>=0.57.0",
-        "matplotlib",
-        "scipy>=1.9.0",
-        "scikit-learn",
-        "cellpose",
-        "scanimage-tiff-reader>=1.4.1"
-        ]
+# ------------------------------------------------------------------------------------
+# NOTE: This is a custom fork of Suite2p, originally by Marius Pachitariu & Carsen Stringer.
+# We preserve their license (GPL v3) and give credit for the original code. 
+# This file has been adapted to clarify it is a modified version. 
+# ------------------------------------------------------------------------------------
+
+install_deps = [
+    "importlib-metadata",
+    "natsort",
+    "rastermap>=0.9.0",
+    "tifffile",
+    "torch>=1.13.1",
+    "numpy>=1.24.3",
+    "numba>=0.57.0",
+    "matplotlib",
+    "scipy>=1.9.0",
+    "scikit-learn",
+    "cellpose",
+    "scanimage-tiff-reader>=1.4.1"
+]
 
 gui_deps = [
-        "qtpy",
-        "pyqt6",
-        "pyqt6.sip",
-        "pyqtgraph",
-      ]
+    "qtpy",
+    "pyqt6",
+    "pyqt6.sip",
+    "pyqtgraph",
+]
 
 io_deps = [
     "paramiko",
@@ -32,82 +39,85 @@ io_deps = [
 ]
 
 nwb_deps = [
-        "pynwb>=2.3.2",
-      ]
-
-test_deps = [
-      "pytest",
-      "tenacity",
-      "tqdm",
-      "pynwb>=2.3.2", #this is needed as test_io contains a test with nwb
-      "pytest-qt>3.3.0",
+    "pynwb>=2.3.2",
 ]
 
-# check if pyqt/pyside already installed
+test_deps = [
+    "pytest",
+    "tenacity",
+    "tqdm",
+    "pynwb>=2.3.2",  # needed since test_io contains an NWB test
+    "pytest-qt>3.3.0",
+]
+
+# Check if PyQt/PySide is installed to avoid conflicts
 try:
     import PyQt5
     gui_deps.remove("pyqt6")
     gui_deps.remove("pyqt6.sip")
-except:
+except ImportError:
     pass
 
 try:
     import PySide2
     gui_deps.remove("pyqt6")
     gui_deps.remove("pyqt6.sip")
-except:
+except ImportError:
     pass
 
 try:
     import PySide6
     gui_deps.remove("pyqt6")
     gui_deps.remove("pyqt6.sip")
-except:
+except ImportError:
     pass
 
-all_deps = gui_deps + nwb_deps + test_deps + io_deps 
+all_deps = gui_deps + nwb_deps + test_deps + io_deps
 
+# Torch version check
 try:
     import torch
-    a = torch.ones(2, 3)
     major_version, minor_version, _ = torch.__version__.split(".")
+    # Optionally remove older constraints if a newer Torch is present
     if major_version == "2" or int(minor_version) >= 6:
         install_deps.remove("torch>=1.6")
-except:
+except ImportError:
     pass
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="suite2p",
-    author="Marius Pachitariu and Carsen Stringer",
-    author_email="marius10p@gmail.com",
-    description="Pipeline for calcium imaging",
+    # Name changed to avoid conflicts with the official suite2p package on PyPI.
+    name="suite2p-custom",  
+    # Include both original and your info. 
+    author="Marius Pachitariu, Carsen Stringer (Original Authors); Fork Maintainer: Ahmed Jamali",
+    author_email="Ahmedj@ntnu.no",  # Replace with your contact if desired
+    description="Pipeline for calcium imaging (Custom Fork of Suite2p)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MouseLand/suite2p",
+    url="https://github.com/AhmedJamali/suite2pR",
     packages=setuptools.find_packages(),
     setup_requires=[
-      "pytest-runner",
-      "setuptools_scm",
+        "pytest-runner",
+        "setuptools_scm",
     ],
     use_scm_version=True,
     install_requires=install_deps,
     tests_require=test_deps,
     extras_require={
-      "docs": [
-        "sphinx>=3.0",
-        "sphinxcontrib-apidoc",
-        "sphinx_rtd_theme",
-        "sphinx-prompt",
-        "sphinx-autodoc-typehints",
-      ],
-      "gui": gui_deps,
-      "nwb": nwb_deps,
-      "io": io_deps,
-      "tests": test_deps,
-      "all": all_deps,
+        "docs": [
+            "sphinx>=3.0",
+            "sphinxcontrib-apidoc",
+            "sphinx_rtd_theme",
+            "sphinx-prompt",
+            "sphinx-autodoc-typehints",
+        ],
+        "gui": gui_deps,
+        "nwb": nwb_deps,
+        "io": io_deps,
+        "tests": test_deps,
+        "all": all_deps,
     },
     include_package_data=True,
     classifiers=[
@@ -115,11 +125,11 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-      entry_points = {
+    entry_points={
         "console_scripts": [
-          "suite2p = suite2p.__main__:main",
-          "reg_metrics = benchmarks.registration_metrics:main",
-          "tiff2scanimage = scripts.make_tiff_scanimage_compatible:main",
+            "suite2p = suite2p.__main__:main",
+            "reg_metrics = benchmarks.registration_metrics:main",
+            "tiff2scanimage = scripts.make_tiff_scanimage_compatible:main",
         ]
-        },
+    },
 )

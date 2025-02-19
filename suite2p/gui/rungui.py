ed47ae68f01de1b89d5a60edc50aec5391d3aeea
AhmedJ <ahmedj@stud.ntnu.no>
2025-02-19 20:15:39 +0100

    added a sci_raw as gui option

--- suite2p/gui/rungui.py
+++ suite2p/gui/rungui.py
@@ -276,7 +276,7 @@ class RunWindow(QDialog):
         self.inputformat = QComboBox()
         [
             self.inputformat.addItem(f)
-            for f in ["tif", "binary", "bruker", "sbx", "h5", "movie", "nd2", "mesoscan", "raw", "dcimg"]
+            for f in ["tif", "binary", "bruker", "sbx", "h5", "movie", "nd2", "mesoscan", "raw", "sci_raw"] # Adapted by Ahmed Jamali: added "sci_raw"
         ]
         self.inputformat.currentTextChanged.connect(self.parse_inputformat)
         self.layout.addWidget(self.inputformat, 2, 0, 1, 1)

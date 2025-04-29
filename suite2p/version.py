6fd5edaa89b21aee0594c78f024934b8f3a727a4
Flynn OConnell <flynnoconnell@gmail.com>
2025-04-29 18:34:32 -0400

    bump ver, deps

--- suite2p/version.py
+++ suite2p/version.py
@@ -1,6 +1,5 @@
 """
 Copyright © 2023 Howard Hughes Medical Institute, Authored by Carsen Stringer and Marius Pachitariu.
 """
-from importlib_metadata import metadata as _metadata
 
-version = _metadata("suite2p")["version"]
+__version__ = "1.0.0"

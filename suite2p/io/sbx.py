7584f57dedd6f02b510a6a20279ca98ac104e4c6
Chris Ki <chriski777@berkeley.edu>
2026-06-09 22:25:41 -0400

    FIxes #1247.

--- suite2p/io/sbx.py
+++ suite2p/io/sbx.py
@@ -8,7 +8,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-from .utils import init_settings, find_files_open_binaries
 
 try:
     from sbxreader import sbx_memmap
@@ -17,38 +16,39 @@ except:
     HAS_SBX = False
     
 
-def sbx_to_binary(settings, ndeadcols=-1, ndeadrows=0):
+def sbx_to_binary(dbs, settings, reg_file, reg_file_chan2):
     """  finds scanbox files and writes them to binaries
 
     Parameters
     ----------
-    settings : dictionary
-        "nplanes", "data_path", "save_path", "save_folder", "fast_disk",
-        "nchannels", "keep_movie_raw", "look_one_level_down"
+    dbs : list of dict
+        Per-plane database dictionaries. Must contain "file_list", "first_files",
+        "nplanes", "nchannels", "batch_size", "functional_chan". Updated in-place
+        with "Ly", "Lx", "nframes", "frames_per_file", "frames_per_folder",
+        "meanImg", and "meanImg_chan2".
+    settings : dict
+        Suite2p settings dictionary.
+    reg_file : list of file objects
+        Opened binary files for writing each plane's functional channel data.
+    reg_file_chan2 : list of file objects
+        Opened binary files for writing each plane's second channel data
+        (used only when nchannels > 1).
 
     Returns
     -------
-        settings : dictionary of first plane
-            "Ly", "Lx", settings["reg_file"] or settings["raw_file"] is created binary
-
+    dbs : list of dict
+        Updated database dictionaries.
     """
     if not HAS_SBX:
         raise ImportError("sbxreader is required for this file type, please 'pip install sbxreader'")
 
-    settings1 = init_settings(settings)
-    # the following should be taken from the metadata and not needed but the files are initialized before...
-    nplanes = settings1[0]["nplanes"]
-    nchannels = settings1[0]["nchannels"]
-    # open all binary files for writing
-    settings1, sbxlist, reg_file, reg_file_chan2 = find_files_open_binaries(settings1)
-    iall = 0
-    for j in range(settings1[0]["nplanes"]):
-        settings1[j]["nframes_per_folder"] = np.zeros(len(sbxlist), np.int32)
-    ik = 0
-    if "sbx_ndeadcols" in settings1[0].keys():
-        ndeadcols = int(settings1[0]["sbx_ndeadcols"])
-    if "sbx_ndeadrows" in settings1[0].keys():
-        ndeadrows = int(settings1[0]["sbx_ndeadrows"])
+    sbxlist = dbs[0]["file_list"]
+    first_files = dbs[0]["first_files"]
+    nplanes = dbs[0]["nplanes"]
+    nchannels = dbs[0]["nchannels"]
+
+    ndeadcols = int(dbs[0].get("sbx_ndeadcols", -1))
+    ndeadrows = int(dbs[0].get("sbx_ndeadrows", 0))
 
     if ndeadcols == -1 or ndeadrows == -1:
         # compute dead rows and cols from the first file
@@ -71,21 +71,23 @@ def sbx_to_binary(settings, ndeadcols=-1, ndeadrows=0):
         logger.info("Removing {0} dead columns while loading sbx data.".format(ndeadcols))
         logger.info("Removing {0} dead rows while loading sbx data.".format(ndeadrows))
 
-    settings1[0]["sbx_ndeadcols"] = ndeadcols
-    settings1[0]["sbx_ndeadrows"] = ndeadrows
+    for j in range(nplanes):
+        dbs[j]["sbx_ndeadcols"] = ndeadcols
+        dbs[j]["sbx_ndeadrows"] = ndeadrows
 
+    iall = 0
     for ifile, sbxfname in enumerate(sbxlist):
         f = sbx_memmap(sbxfname)
         nplanes = f.shape[1]
         nchannels = f.shape[2]
         nframes = f.shape[0]
-        iblocks = np.arange(0, nframes, settings1[0]["batch_size"])
+        iblocks = np.arange(0, nframes, dbs[0]["batch_size"])
         if iblocks[-1] < nframes:
             iblocks = np.append(iblocks, nframes)
 
         # data = nframes x nplanes x nchannels x pixels x pixels
         if nchannels > 1:
-            nfunc = settings1[0]["functional_chan"] - 1
+            nfunc = dbs[0]["functional_chan"] - 1
         else:
             nfunc = 0
         # loop over all frames
@@ -95,46 +97,30 @@ def sbx_to_binary(settings, ndeadcols=-1, ndeadrows=0):
             im = im.astype(np.int16)
             im2mean = im.mean(axis=0).astype(np.float32) / len(iblocks)
             for ichan in range(nchannels):
-                nframes = im.shape[0]
+                nframes_chunk = im.shape[0]
                 im2write = im[:, :, ichan, :, :]
                 for j in range(0, nplanes):
                     if iall == 0:
-                        settings1[j]["meanImg"] = np.zeros((im.shape[3], im.shape[4]),
-                                                      np.float32)
+                        dbs[j]["Ly"] = im.shape[3]
+                        dbs[j]["Lx"] = im.shape[4]
+                        dbs[j]["meanImg"] = np.zeros((im.shape[3], im.shape[4]), np.float32)
                         if nchannels > 1:
-                            settings1[j]["meanImg_chan2"] = np.zeros(
+                            dbs[j]["meanImg_chan2"] = np.zeros(
                                 (im.shape[3], im.shape[4]), np.float32)
-                        settings1[j]["nframes"] = 0
+                        dbs[j]["nframes"] = 0
+                        dbs[j]["frames_per_file"] = np.zeros(len(sbxlist), np.int32)
+                        dbs[j]["frames_per_folder"] = np.zeros(first_files.sum(), np.int32)
                     if ichan == nfunc:
-                        settings1[j]["meanImg"] += np.squeeze(im2mean[j, ichan, :, :])
+                        dbs[j]["meanImg"] += np.squeeze(im2mean[j, ichan, :, :])
                         reg_file[j].write(
                             bytearray(im2write[:, j, :, :].astype("int16")))
                     else:
-                        settings1[j]["meanImg_chan2"] += np.squeeze(im2mean[j, ichan, :, :])
+                        dbs[j]["meanImg_chan2"] += np.squeeze(im2mean[j, ichan, :, :])
                         reg_file_chan2[j].write(
                             bytearray(im2write[:, j, :, :].astype("int16")))
 
-                    settings1[j]["nframes"] += im2write.shape[0]
-                    settings1[j]["nframes_per_folder"][ifile] += im2write.shape[0]
-            ik += nframes
-            iall += nframes
+                    dbs[j]["nframes"] += nframes_chunk
+                    dbs[j]["frames_per_file"][ifile] += nframes_chunk
+            iall += im.shape[0]
 
-    # write settings files
-    do_registration = settings1[0]["do_registration"]
-    do_nonrigid = settings1[0]["nonrigid"]
-    for settings in settings1:
-        settings["Ly"] = im.shape[3]
-        settings["Lx"] = im.shape[4]
-        if not do_registration:
-            settings["yrange"] = np.array([0, settings["Ly"]])
-            settings["xrange"] = np.array([0, settings["Lx"]])
-        #settings["meanImg"] /= settings["nframes"]
-        #if nchannels>1:
-        #    settings["meanImg_chan2"] /= settings["nframes"]
-        np.save(settings["settings_path"], settings)
-    # close all binary files and write settings files
-    for j in range(0, nplanes):
-        reg_file[j].close()
-        if nchannels > 1:
-            reg_file_chan2[j].close()
-    return settings1[0]
+    return dbs
